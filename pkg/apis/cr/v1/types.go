673a732862b01d3500e72ae6763b5a62cb149192
Zhe Jin <jinzhej@cn.ibm.com>
2017-08-09 19:34:26 +0800

    create CRD definition of ResourceQuotaAllocator

---
+++ pkg/apis/cr/v1/types.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+const ResourceQuotaAllocatorPlural = "resourcequotaallocators"
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ResourceQuotaAllocator struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              ResourceQuotaAllocatorSpec   `json:"spec"`
+	Status            ResourceQuotaAllocatorStatus `json:"status,omitempty"`
+}
+
+type ResourceQuotaAllocatorSpec struct {
+	Share map[string]intstr.IntOrString `json:"share"`
+}
+
+type ResourceQuotaAllocatorStatus struct {
+	Share map[string]intstr.IntOrString  `json:"share"`
+	Usage map[string]ResourceList    `json:"usage"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ResourceQuotaAllocatorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []ResourceQuotaAllocator `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ResourceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Resources map[ResourceName]resource.Quantity `json:"resources"`
+}
+
+type ResourceName string
