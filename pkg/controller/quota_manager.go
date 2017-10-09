a06951b0708bb8682b4fb0efe6dbdba1ada7ca50
Zhe Jin <jinzhej@cn.ibm.com>
2017-10-09 17:46:41 +0800

    refine quotamanage and proportion as design doc

--- pkg/controller/quota_manager.go
+++ pkg/controller/quota_manager.go
@@ -17,106 +17,86 @@ limitations under the License.
 package controller
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/golang/glog"
-	"github.com/kubernetes-incubator/kube-arbitrator/pkg/schedulercache"
+	apiv1 "github.com/kubernetes-incubator/kube-arbitrator/pkg/apis/v1"
+	"github.com/kubernetes-incubator/kube-arbitrator/pkg/client"
 
 	"k8s.io/apimachinery/pkg/api/resource"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	clientcache "k8s.io/client-go/tools/cache"
 )
 
 type quotaManager struct {
 	config *rest.Config
-	queue  *clientcache.FIFO
 }
 
-type updatedResource struct {
-	ns     string
-	cpu    int64
-	memory int64
-}
-
-func quotaKeyFunc(obj interface{}) (string, error) {
-	res, ok := obj.(updatedResource)
-	if !ok {
-		return "", fmt.Errorf("obj is not updatedResource type: %v", obj)
-	}
-	return res.ns, nil
-}
-
-// updateQuota add update request based on ResourceQuotaAllocator to queue
-func (qm *quotaManager) updateQuota(queue *schedulercache.QueueInfo) {
-	if queue == nil {
-		return
-	}
-
-	res := updatedResource{
-		ns: queue.Queue().Namespace,
-	}
-	for k, v := range queue.Queue().Status.Deserved.Resources {
-		switch k {
-		case "cpu":
-			if cpu, ok := v.AsInt64(); ok {
-				res.cpu = cpu
-			} else {
-				glog.Errorf("cannot parse share cpu %#v", v)
-			}
-		case "memory":
-			if memory, ok := v.AsInt64(); ok {
-				res.memory = memory
-			} else {
-				glog.Errorf("cannot parse share memory %#v", v)
-			}
-		}
-	}
-
-	qm.queue.Add(res)
+func (qm *quotaManager) Run() {
+	wait.Until(qm.runOnce, 2*time.Second, wait.NeverStop)
 }
 
 // run get request from queue and update to Quota
-func (qm *quotaManager) run() {
+func (qm *quotaManager) runOnce() {
+	queues, err := qm.fetchAllQueue()
+	if err != nil {
+		glog.Error("fail to fetch all queue info")
+		return
+	}
+
+	qm.updateQuotas(queues)
+}
+
+func (qm *quotaManager) fetchAllQueue() ([]apiv1.Queue, error) {
+	queueClient, _, err := client.NewClient(qm.config)
+	if err != nil {
+		return nil, err
+	}
+
+	queueList := apiv1.QueueList{}
+	err = queueClient.Get().Resource(apiv1.QueuePlural).Do().Into(&queueList)
+	if err != nil {
+		return nil, err
+	}
+
+	return queueList.Items, nil
+}
+
+func (qm *quotaManager) updateQuotas(queues []apiv1.Queue) {
 	cs := kubernetes.NewForConfigOrDie(qm.config)
 
-	processToUpdateRQ := func(obj interface{}) error {
-		res, ok := obj.(updatedResource)
-		if !ok {
-			return fmt.Errorf("cannot convert obj %#v to updatedResource", obj)
-		}
+	for _, queue := range queues {
+		rqController := cs.CoreV1().ResourceQuotas(queue.Namespace)
 
-		rqController := cs.CoreV1().ResourceQuotas(res.ns)
 		var options meta_v1.ListOptions
 		rqList, err := rqController.List(options)
 		if len(rqList.Items) != 1 || err != nil {
-			return fmt.Errorf("more than one resourceQuota or ecounter an error")
+			glog.Errorf("more than one resourceQuota or ecounter an error, namespace %s", queue.Namespace)
+			continue
 		}
 
-		for _, rq := range rqList.Items {
-			updatedRq := rq.DeepCopy()
-			cpuQuota := *resource.NewQuantity(res.cpu, resource.DecimalSI)
-			memoryQuota := *resource.NewQuantity(res.memory, resource.BinarySI)
-			updatedRq.Spec.Hard["limits.cpu"] = cpuQuota
-			updatedRq.Spec.Hard["requests.cpu"] = cpuQuota
-			updatedRq.Spec.Hard["limits.memory"] = memoryQuota
-			updatedRq.Spec.Hard["requests.memory"] = memoryQuota
-
-			_, err := rqController.Update(updatedRq)
-			if err != nil {
-				return fmt.Errorf("failed to update resource quota")
+		updatedRq := rqList.Items[0].DeepCopy()
+		if cpuQuantity, ok := queue.Status.Deserved.Resources["cpu"]; ok {
+			if cpu, ok := (&cpuQuantity).AsInt64(); ok {
+				cpuQuota := *resource.NewQuantity(cpu, resource.DecimalSI)
+				updatedRq.Spec.Hard["limits.cpu"] = cpuQuota
+				updatedRq.Spec.Hard["requests.cpu"] = cpuQuota
 			}
 		}
-		return nil
-	}
+		if memoryQuantity, ok := queue.Status.Deserved.Resources["memory"]; ok {
+			if memory, ok := (&memoryQuantity).AsInt64(); ok {
+				memoryQuota := *resource.NewQuantity(memory, resource.BinarySI)
+				updatedRq.Spec.Hard["limits.memory"] = memoryQuota
+				updatedRq.Spec.Hard["requests.memory"] = memoryQuota
+			}
+		}
 
-	for {
-		_, err := qm.queue.Pop(processToUpdateRQ)
+		_, err = rqController.Update(updatedRq)
 		if err != nil {
-			glog.Error(err)
+			glog.Errorf("failed to update resource quota")
+			continue
 		}
 	}
-
-	panic("unreachable!")
 }
