5056a33bf288d2d144e867a596878704ab9ee63a
Zhe Jin <jinzhej@cn.ibm.com>
2017-11-02 11:22:59 +0800

    code refine

--- pkg/schedulercache/queue_info.go
+++ pkg/schedulercache/queue_info.go
@@ -29,19 +29,24 @@ type QueueInfo struct {
 	Pods  map[string]*v1.Pod
 }
 
-// true  - all resources(cpu/memory) in res1 < res2
-// false - not above case
-func compareResources(res1 map[apiv1.ResourceName]resource.Quantity, res2 map[apiv1.ResourceName]resource.Quantity) bool {
+// -1  - if res1 < res2
+// 0   - if res1 = res2
+// 1   - if not belong above cases
+func CompareResources(res1 map[apiv1.ResourceName]resource.Quantity, res2 map[apiv1.ResourceName]resource.Quantity) int {
 	cpu1 := res1["cpu"].DeepCopy()
 	cpu2 := res2["cpu"].DeepCopy()
 	memory1 := res1["memory"].DeepCopy()
 	memory2 := res2["memory"].DeepCopy()
 
 	if cpu1.Cmp(cpu2) <= 0 && memory1.Cmp(memory2) <= 0 {
-		return true
+		if cpu1.Cmp(cpu2) == 0 && memory1.Cmp(memory2) == 0 {
+			return 0
+		} else {
+			return -1
+		}
+	} else {
+		return 1
 	}
-
-	return false
 }
 
 func (r *QueueInfo) Name() string {
@@ -53,11 +58,11 @@ func (r *QueueInfo) Queue() *apiv1.Queue {
 }
 
 func (r *QueueInfo) UsedUnderAllocated() bool {
-	return compareResources(r.queue.Status.Used.Resources, r.queue.Status.Allocated.Resources)
+	return (CompareResources(r.queue.Status.Used.Resources, r.queue.Status.Allocated.Resources) <= 0)
 }
 
 func (r *QueueInfo) UsedUnderDeserved() bool {
-	return compareResources(r.queue.Status.Used.Resources, r.queue.Status.Deserved.Resources)
+	return (CompareResources(r.queue.Status.Used.Resources, r.queue.Status.Deserved.Resources) <= 0)
 }
 
 func (r *QueueInfo) Clone() *QueueInfo {
