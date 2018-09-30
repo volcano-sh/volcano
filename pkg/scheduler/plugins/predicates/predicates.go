e900251cfb33898c7a9f7679b4ec4affa34e89d1
Da K. Ma <klaus1982.cn@gmail.com>
2018-09-30 14:16:16 +0800

    Added Toleration/Taint predicate.
    Signed-off-by: Da K. Ma <klaus1982.cn@gmail.com>

--- pkg/scheduler/plugins/predicates/predicates.go
+++ pkg/scheduler/plugins/predicates/predicates.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package nodeaffinity
+package predicates
 
 import (
 	"fmt"
@@ -42,16 +42,46 @@ func (pp *nodeAffinityPlugin) OnSessionOpen(ssn *framework.Session) {
 	ssn.AddPredicateFn(func(task *api.TaskInfo, node *api.NodeInfo) error {
 		nodeInfo := cache.NewNodeInfo(node.Pods()...)
 		nodeInfo.SetNode(node.Node)
+
+		// NodeSeletor Predicate
 		fit, _, err := predicates.PodMatchNodeSelector(task.Pod, nil, nodeInfo)
 		if err != nil {
 			return err
 		}
 
-		glog.V(3).Infof("Predicates Task <%s/%s> on Node <%s>: fit %t, err %v",
+		glog.V(3).Infof("NodeSelect predicates Task <%s/%s> on Node <%s>: fit %t, err %v",
 			task.Namespace, task.Name, node.Name, fit, err)
 
 		if !fit {
 			return fmt.Errorf("node <%s> didn't match task <%s/%s> node selector",
+				node.Name, task.Namespace, task.Name)
+		}
+
+		// HostPorts Predicate
+		fit, _, err = predicates.PodFitsHostPorts(task.Pod, nil, nodeInfo)
+		if err != nil {
+			return err
+		}
+
+		glog.V(3).Infof("HostPorts predicates Task <%s/%s> on Node <%s>: fit %t, err %v",
+			task.Namespace, task.Name, node.Name, fit, err)
+
+		if !fit {
+			return fmt.Errorf("node <%s> didn't have available host ports for task <%s/%s>",
+				node.Name, task.Namespace, task.Name)
+		}
+
+		// Toleration/Taint Predicate
+		fit, _, err = predicates.PodToleratesNodeTaints(task.Pod, nil, nodeInfo)
+		if err != nil {
+			return err
+		}
+
+		glog.V(3).Infof("Toleration/Taint predicates Task <%s/%s> on Node <%s>: fit %t, err %v",
+			task.Namespace, task.Name, node.Name, fit, err)
+
+		if !fit {
+			return fmt.Errorf("task <%s/%s> does not tolerate node <%s> taints",
 				node.Name, task.Namespace, task.Name)
 		}
 
