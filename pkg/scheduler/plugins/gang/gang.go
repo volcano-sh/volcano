461a695e3b199ea49ec0e81d8575d0d37aa1cf5e
Adam Marek <adam.marek@intel.com>
2018-11-05 10:51:58 +0100

    Formatting corrections

--- pkg/scheduler/plugins/gang/gang.go
+++ pkg/scheduler/plugins/gang/gang.go
@@ -122,7 +122,7 @@ func (gp *gangPlugin) OnSessionOpen(ssn *framework.Session) {
 		if !lReady && !rReady {
 			if lv.CreationTimestamp.Equal(&rv.CreationTimestamp) {
 				if lv.UID < rv.UID {
-					return -1;
+					return -1
 				}
 			} else if lv.CreationTimestamp.Before(&rv.CreationTimestamp) {
 				return -1
