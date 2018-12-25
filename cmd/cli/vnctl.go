9e001f3bae9195cc7d6040a62e680242c2e68fef
Kevin <wangzefeng@huawei.com>
2018-12-25 15:00:54 +0800

    Fix build error

--- cmd/cli/vnctl.go
+++ cmd/cli/vnctl.go
@@ -25,7 +25,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/wait"
 
-	"volcanoproj.org/volcano/pkg/cli/job"
+	"hpw.cloud/volcano/pkg/cli/job"
 )
 
 var logFlushFreq = pflag.Duration("log-flush-frequency", 5*time.Second, "Maximum number of seconds between log flushes")
