e5858947fecf78d121d21de1a026e303e8740fbc
zhaoshuhao <zhaoshuhao@huawei.com>
2019-01-30 21:58:54 +0800

    fix comments

---
+++ cmd/admission-controller/app/server.go
@@ -0,0 +1,109 @@
+/*
+Copyright 2018 The Volcano Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package app
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/golang/glog"
+
+	"k8s.io/api/admission/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	appConf "hpw.cloud/volcano/cmd/admission-controller/app/configure"
+	admissioncontroller "hpw.cloud/volcano/pkg/admission-controller"
+)
+
+const (
+	CONTENTTYPE     = "Content-Type"
+	APPLICATIONJSON = "application/json"
+)
+
+// Get a clientset with in-cluster config.
+func GetClient() *kubernetes.Clientset {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		glog.Fatal(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	return clientset
+}
+
+func ConfigTLS(config *appConf.Config, clientset *kubernetes.Clientset) *tls.Config {
+	sCert, err := tls.LoadX509KeyPair(config.CertFile, config.KeyFile)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{sCert},
+	}
+}
+
+func Serve(w http.ResponseWriter, r *http.Request, admit admissioncontroller.AdmitFunc) {
+	var body []byte
+	if r.Body != nil {
+		if data, err := ioutil.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
+
+	// verify the content type is accurate
+	contentType := r.Header.Get(CONTENTTYPE)
+	if contentType != APPLICATIONJSON {
+		glog.Errorf("contentType=%s, expect application/json", contentType)
+		return
+	}
+
+	var reviewResponse *v1beta1.AdmissionResponse
+	ar := v1beta1.AdmissionReview{}
+	deserializer := admissioncontroller.Codecs.UniversalDeserializer()
+	if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
+		reviewResponse = admissioncontroller.ToAdmissionResponse(err)
+	} else {
+		reviewResponse = admit(ar)
+	}
+	glog.V(3).Infof("sending response: %v", reviewResponse)
+
+	response := createResponse(reviewResponse, &ar)
+	resp, err := json.Marshal(response)
+	if err != nil {
+		glog.Error(err)
+	}
+	if _, err := w.Write(resp); err != nil {
+		glog.Error(err)
+	}
+}
+
+func createResponse(reviewResponse *v1beta1.AdmissionResponse, ar *v1beta1.AdmissionReview) v1beta1.AdmissionReview {
+	response := v1beta1.AdmissionReview{}
+	if reviewResponse != nil {
+		response.Response = reviewResponse
+		response.Response.UID = ar.Request.UID
+	}
+	// reset the Object and OldObject, they are not needed in a response.
+	ar.Request.Object = runtime.RawExtension{}
+	ar.Request.OldObject = runtime.RawExtension{}
+
+	return response
+}
--- cmd/admission-controller/main.go
+++ cmd/admission-controller/main.go
@@ -16,93 +16,34 @@ limitations under the License.
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"net/http"
+	"strconv"
 
-	"github.com/golang/glog"
-	"k8s.io/api/admission/v1beta1"
-	"k8s.io/apimachinery/pkg/runtime"
+	"hpw.cloud/volcano/cmd/admission-controller/app"
+	appConf "hpw.cloud/volcano/cmd/admission-controller/app/configure"
 	admissioncontroller "hpw.cloud/volcano/pkg/admission-controller"
 )
 
-
-// Config contains the server (the admission controller) cert and key.
-type Config struct {
-	CertFile string
-	KeyFile  string
-}
-
-func (c *Config) addFlags() {
-	flag.StringVar(&c.CertFile, "tls-cert-file", c.CertFile, ""+
-		"File containing the default x509 Certificate for HTTPS. (CA cert, if any, concatenated "+
-		"after server cert).")
-	flag.StringVar(&c.KeyFile, "tls-private-key-file", c.KeyFile, ""+
-		"File containing the default x509 private key matching --tls-cert-file.")
-}
-
-func serve(w http.ResponseWriter, r *http.Request, admit admissioncontroller.AdmitFunc) {
-	var body []byte
-	if r.Body != nil {
-		if data, err := ioutil.ReadAll(r.Body); err == nil {
-			body = data
-		}
-	}
-
-	// verify the content type is accurate
-	contentType := r.Header.Get("Content-Type")
-	if contentType != "application/json" {
-		glog.Errorf("contentType=%s, expect application/json", contentType)
-		return
-	}
-
-	glog.V(3).Infof(fmt.Sprintf("handling request: %v", body))
-	var reviewResponse *v1beta1.AdmissionResponse
-	ar := v1beta1.AdmissionReview{}
-	deserializer := admissioncontroller.Codecs.UniversalDeserializer()
-	if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
-		reviewResponse = admissioncontroller.ToAdmissionResponse(err)
-	} else {
-		reviewResponse = admit(ar)
-	}
-	glog.V(3).Infof(fmt.Sprintf("sending response: %v", reviewResponse))
-
-	response := v1beta1.AdmissionReview{}
-	if reviewResponse != nil {
-		response.Response = reviewResponse
-		response.Response.UID = ar.Request.UID
-	}
-	// reset the Object and OldObject, they are not needed in a response.
-	ar.Request.Object = runtime.RawExtension{}
-	ar.Request.OldObject = runtime.RawExtension{}
-
-	resp, err := json.Marshal(response)
-	if err != nil {
-		glog.Error(err)
-	}
-	if _, err := w.Write(resp); err != nil {
-		glog.Error(err)
-	}
-}
-
 func serveJobs(w http.ResponseWriter, r *http.Request) {
-	serve(w, r, admissioncontroller.AdmitJobs)
+	app.Serve(w, r, admissioncontroller.AdmitJobs)
 }
-
 
 func main() {
-	var config Config
-	config.addFlags()
+	config := appConf.NewConfig()
+	config.AddFlags()
 	flag.Parse()
 
 	http.HandleFunc(admissioncontroller.AdmitJobPath, serveJobs)
 
-	clientset := getClient()
+	addr := ":443"
+	if config.Port > 0 && config.Port < 65536 {
+		addr = ":" + strconv.Itoa(config.Port)
+	}
+	clientset := app.GetClient()
 	server := &http.Server{
-		Addr:      ":443",
-		TLSConfig: configTLS(config, clientset),
+		Addr:      addr,
+		TLSConfig: app.ConfigTLS(config, clientset),
 	}
 	server.ListenAndServeTLS("", "")
 }
