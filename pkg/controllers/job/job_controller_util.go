9ba8eefe8a8bfc2847f9b1ca9fa4f0f74ba66b6d
TommyLike <hu.husheng@huawei.com>
2019-03-15 15:47:56 +0800

    Add more codes

--- pkg/controllers/job/job_controller_util.go
+++ pkg/controllers/job/job_controller_util.go
@@ -18,30 +18,18 @@ package job
 
 import (
 	"fmt"
+	"github.com/golang/glog"
 
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	kbapi "github.com/kubernetes-sigs/kube-batch/pkg/apis/scheduling/v1alpha1"
 
+	admissioncontroller "volcano.sh/volcano/pkg/admission"
 	vkv1 "volcano.sh/volcano/pkg/apis/batch/v1alpha1"
 	"volcano.sh/volcano/pkg/apis/helpers"
 	"volcano.sh/volcano/pkg/controllers/job/apis"
 )
-
-func validate(job *vkv1.Job) error {
-	tsNames := map[string]string{}
-
-	for _, ts := range job.Spec.Tasks {
-		if _, found := tsNames[ts.Template.Name]; found {
-			return fmt.Errorf("duplicated TaskSpec")
-		}
-
-		tsNames[ts.Template.Name] = ts.Template.Name
-	}
-
-	return nil
-}
 
 func eventKey(obj interface{}) interface{} {
 	req, ok := obj.(apis.Request)
@@ -55,12 +43,16 @@ func eventKey(obj interface{}) interface{} {
 	}
 }
 
+func MakePodName(jobName string, taskName string, index int) string {
+	return fmt.Sprintf(TaskNameFmt, jobName, taskName, index)
+}
+
 func createJobPod(job *vkv1.Job, template *v1.PodTemplateSpec, ix int) *v1.Pod {
 	templateCopy := template.DeepCopy()
 
 	pod := &v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf(TaskNameFmt, job.Name, template.Name, ix),
+			Name:      MakePodName(job.Name, template.Name, ix),
 			Namespace: job.Namespace,
 			OwnerReferences: []metav1.OwnerReference{
 				*metav1.NewControllerRef(job, helpers.JobKind),
@@ -76,19 +68,21 @@ func createJobPod(job *vkv1.Job, template *v1.PodTemplateSpec, ix int) *v1.Pod {
 		pod.Spec.SchedulerName = job.Spec.SchedulerName
 	}
 
+	inputPVC := job.Annotations[admissioncontroller.PVCInputName]
+	outputPVC := job.Annotations[admissioncontroller.PVCOutputName]
 	if job.Spec.Output != nil {
 		if job.Spec.Output.VolumeClaim == nil {
 			volume := v1.Volume{
-				Name: fmt.Sprintf("%s-output", job.Name),
+				Name: outputPVC,
 			}
 			volume.EmptyDir = &v1.EmptyDirVolumeSource{}
 			pod.Spec.Volumes = append(pod.Spec.Volumes, volume)
 		} else {
 			volume := v1.Volume{
-				Name: fmt.Sprintf("%s-output", job.Name),
+				Name: outputPVC,
 			}
 			volume.PersistentVolumeClaim = &v1.PersistentVolumeClaimVolumeSource{
-				ClaimName: fmt.Sprintf("%s-output", job.Name),
+				ClaimName: outputPVC,
 			}
 
 			pod.Spec.Volumes = append(pod.Spec.Volumes, volume)
@@ -97,7 +91,7 @@ func createJobPod(job *vkv1.Job, template *v1.PodTemplateSpec, ix int) *v1.Pod {
 		for i, c := range pod.Spec.Containers {
 			vm := v1.VolumeMount{
 				MountPath: job.Spec.Output.MountPath,
-				Name:      fmt.Sprintf("%s-output", job.Name),
+				Name:      outputPVC,
 			}
 			pod.Spec.Containers[i].VolumeMounts = append(c.VolumeMounts, vm)
 		}
@@ -106,16 +100,16 @@ func createJobPod(job *vkv1.Job, template *v1.PodTemplateSpec, ix int) *v1.Pod {
 	if job.Spec.Input != nil {
 		if job.Spec.Input.VolumeClaim == nil {
 			volume := v1.Volume{
-				Name: fmt.Sprintf("%s-input", job.Name),
+				Name: inputPVC,
 			}
 			volume.EmptyDir = &v1.EmptyDirVolumeSource{}
 			pod.Spec.Volumes = append(pod.Spec.Volumes, volume)
 		} else {
 			volume := v1.Volume{
-				Name: fmt.Sprintf("%s-input", job.Name),
+				Name: inputPVC,
 			}
 			volume.PersistentVolumeClaim = &v1.PersistentVolumeClaimVolumeSource{
-				ClaimName: fmt.Sprintf("%s-input", job.Name),
+				ClaimName: inputPVC,
 			}
 
 			pod.Spec.Volumes = append(pod.Spec.Volumes, volume)
@@ -124,7 +118,7 @@ func createJobPod(job *vkv1.Job, template *v1.PodTemplateSpec, ix int) *v1.Pod {
 		for i, c := range pod.Spec.Containers {
 			vm := v1.VolumeMount{
 				MountPath: job.Spec.Input.MountPath,
-				Name:      fmt.Sprintf("%s-input", job.Name),
+				Name:      inputPVC,
 			}
 
 			pod.Spec.Containers[i].VolumeMounts = append(c.VolumeMounts, vm)
@@ -140,14 +134,15 @@ func createJobPod(job *vkv1.Job, template *v1.PodTemplateSpec, ix int) *v1.Pod {
 	if len(tsKey) == 0 {
 		tsKey = vkv1.DefaultTaskSpec
 	}
-	pod.Annotations[vkv1.TaskSpecKey] = tsKey
 
 	if len(pod.Annotations) == 0 {
 		pod.Annotations = make(map[string]string)
 	}
 
+	pod.Annotations[vkv1.TaskSpecKey] = tsKey
 	pod.Annotations[kbapi.GroupNameAnnotationKey] = job.Name
 	pod.Annotations[vkv1.JobNameKey] = job.Name
+	pod.Annotations[vkv1.JobVersion] = fmt.Sprintf("%d", job.Status.Version)
 
 	if len(pod.Labels) == 0 {
 		pod.Labels = make(map[string]string)
@@ -168,6 +163,16 @@ func createJobPod(job *vkv1.Job, template *v1.PodTemplateSpec, ix int) *v1.Pod {
 func applyPolicies(job *vkv1.Job, req *apis.Request) vkv1.Action {
 	if len(req.Action) != 0 {
 		return req.Action
+	}
+
+	//For all the requests triggered from discarded job resources will perform sync action instead
+	if req.JobVersion > 0 && req.JobVersion < job.Status.Version {
+		glog.Infof("Request %s is outdated, will perform sync instead.", req)
+		return vkv1.SyncJobAction
+	}
+
+	if req.Event == vkv1.OutOfSyncEvent {
+		return vkv1.SyncJobAction
 	}
 
 	// Overwrite Job level policies
