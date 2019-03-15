d069a83343aba0cfc1bd45f3c1be9ca2910421ba
TommyLike <hu.husheng@huawei.com>
2019-03-15 14:54:02 +0800

    Support helm chart

--- cmd/controllers/app/server.go
+++ cmd/controllers/app/server.go
@@ -60,10 +60,10 @@ func Run(opt *options.ServerOption) error {
 		return err
 	}
 
-	JobController := job.NewJobController(config)
+	jobController := job.NewJobController(config)
 
 	run := func(ctx context.Context) {
-		JobController.Run(ctx.Done())
+		jobController.Run(ctx.Done())
 		<-ctx.Done()
 	}
 
