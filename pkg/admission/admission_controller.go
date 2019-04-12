a24a4a3ae238c1ecbd283f01e8ad04716e57acd5
Zhang Jinghui <zhangjinghui5@huawei.com>
2019-04-12 14:11:46 +0800

    add validation of event and action in policies

--- pkg/admission/admission_controller.go
+++ pkg/admission/admission_controller.go
@@ -27,6 +27,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 
 	v1alpha1 "volcano.sh/volcano/pkg/apis/batch/v1alpha1"
 )
@@ -42,6 +43,27 @@ type AdmitFunc func(v1beta1.AdmissionReview) *v1beta1.AdmissionResponse
 
 var scheme = runtime.NewScheme()
 var Codecs = serializer.NewCodecFactory(scheme)
+
+// policyEventMap defines all policy events and whether to allow external use
+var policyEventMap = map[v1alpha1.Event]bool{
+	v1alpha1.AnyEvent:           true,
+	v1alpha1.PodFailedEvent:     true,
+	v1alpha1.PodEvictedEvent:    true,
+	v1alpha1.JobUnknownEvent:    true,
+	v1alpha1.TaskCompletedEvent: true,
+	v1alpha1.OutOfSyncEvent:     false,
+	v1alpha1.CommandIssuedEvent: false,
+}
+
+// policyActionMap defines all policy actions and whether to allow external use
+var policyActionMap = map[v1alpha1.Action]bool{
+	v1alpha1.AbortJobAction:     true,
+	v1alpha1.RestartJobAction:   true,
+	v1alpha1.TerminateJobAction: true,
+	v1alpha1.CompleteJobAction:  true,
+	v1alpha1.ResumeJobAction:    true,
+	v1alpha1.SyncJobAction:      false,
+}
 
 func init() {
 	addToScheme(scheme)
@@ -101,4 +123,41 @@ func DecodeJob(object runtime.RawExtension, resource metav1.GroupVersionResource
 	glog.V(3).Infof("the job struct is %+v", job)
 
 	return job, nil
+}
+
+func validatePolicies(policies []v1alpha1.LifecyclePolicy, fldPath *field.Path) error {
+	errs := field.ErrorList{}
+	for _, p := range policies {
+		if allow, ok := policyEventMap[p.Event]; !ok || !allow {
+			errs = append(errs, field.Invalid(fldPath, p.Event, fmt.Sprintf("invalid policy event")))
+		}
+
+		if allow, ok := policyActionMap[p.Action]; !ok || !allow {
+			errs = append(errs, field.Invalid(fldPath, p.Action, fmt.Sprintf("invalid policy action")))
+		}
+	}
+
+	return errs.ToAggregate()
+}
+
+func getValidEvents() []v1alpha1.Event {
+	var events []v1alpha1.Event
+	for e, allow := range policyEventMap {
+		if allow {
+			events = append(events, e)
+		}
+	}
+
+	return events
+}
+
+func getValidActions() []v1alpha1.Action {
+	var actions []v1alpha1.Action
+	for a, allow := range policyActionMap {
+		if allow {
+			actions = append(actions, a)
+		}
+	}
+
+	return actions
 }
