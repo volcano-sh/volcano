50b759514fde19c21a16eb9349732f5132e0496d
Rajadeepan D Ramesh <rajadeepan.d.ramesh@huawei.com>
2019-07-02 18:00:38 +0530

    Support multiple events in the lifecycle policy

--- pkg/controllers/job/job_controller_util.go
+++ pkg/controllers/job/job_controller_util.go
@@ -25,6 +25,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kbapi "volcano.sh/volcano/pkg/apis/scheduling/v1alpha1"
 
+	"volcano.sh/volcano/pkg/apis/batch/v1alpha1"
 	vkv1 "volcano.sh/volcano/pkg/apis/batch/v1alpha1"
 	"volcano.sh/volcano/pkg/apis/helpers"
 	"volcano.sh/volcano/pkg/controllers/apis"
@@ -143,8 +144,10 @@ func applyPolicies(job *vkv1.Job, req *apis.Request) vkv1.Action {
 		for _, task := range job.Spec.Tasks {
 			if task.Name == req.TaskName {
 				for _, policy := range task.Policies {
-					if len(policy.Event) > 0 && len(req.Event) > 0 {
-						if policy.Event == req.Event || policy.Event == vkv1.AnyEvent {
+					policyEvents := getEventlist(policy)
+
+					if len(policyEvents) > 0 && len(req.Event) > 0 {
+						if checkEventExist(policyEvents, req.Event) || checkEventExist(policyEvents, vkv1.AnyEvent) {
 							return policy.Action
 						}
 					}
@@ -161,8 +164,10 @@ func applyPolicies(job *vkv1.Job, req *apis.Request) vkv1.Action {
 
 	// Parse Job level policies
 	for _, policy := range job.Spec.Policies {
-		if len(policy.Event) > 0 && len(req.Event) > 0 {
-			if policy.Event == req.Event || policy.Event == vkv1.AnyEvent {
+		policyEvents := getEventlist(policy)
+
+		if len(policyEvents) > 0 && len(req.Event) > 0 {
+			if checkEventExist(policyEvents, req.Event) || checkEventExist(policyEvents, vkv1.AnyEvent) {
 				return policy.Action
 			}
 		}
@@ -176,8 +181,27 @@ func applyPolicies(job *vkv1.Job, req *apis.Request) vkv1.Action {
 	return vkv1.SyncJobAction
 }
 
+func getEventlist(policy v1alpha1.LifecyclePolicy) []v1alpha1.Event {
+	policyEventsList := policy.Events
+	if len(policy.Event) > 0 {
+		policyEventsList = append(policyEventsList, policy.Event)
+	}
+	return policyEventsList
+}
+
+func checkEventExist(policyEvents []v1alpha1.Event, reqEvent v1alpha1.Event) bool {
+	for _, event := range policyEvents {
+		if event == reqEvent {
+			return true
+		}
+	}
+	return false
+
+}
+
 func addResourceList(list, req, limit v1.ResourceList) {
 	for name, quantity := range req {
+
 		if value, ok := list[name]; !ok {
 			list[name] = *quantity.Copy()
 		} else {
