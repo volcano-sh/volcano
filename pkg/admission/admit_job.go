a72fd899cad3b6f2b8fbc7f5a432b4a1ff0783b5
xuzhonghu <xuzhonghu@huawei.com>
2019-08-19 14:38:46 +0800

    add comments

--- pkg/admission/admit_job.go
+++ pkg/admission/admit_job.go
@@ -173,7 +173,8 @@ func validateTaskTemplate(task v1alpha1.TaskSpec, job v1alpha1.Job, index int) s
 	var coreTemplateSpec k8score.PodTemplateSpec
 	k8scorev1.Convert_v1_PodTemplateSpec_To_core_PodTemplateSpec(&v1PodTemplate.Template, &coreTemplateSpec, nil)
 
-	// Skip verify container SecurityContex.Privileged
+	// Skip verify container SecurityContex.Privileged as it depends on
+	// the kube-apiserver `allow-privileged` flag.
 	for i, container := range coreTemplateSpec.Spec.Containers {
 		if container.SecurityContext != nil && container.SecurityContext.Privileged != nil {
 			coreTemplateSpec.Spec.Containers[i].SecurityContext.Privileged = nil
