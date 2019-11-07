4e501f7a0767e0c6b163125d3593c0ff54ca3f83
xuzhonghu <xuzhonghu@huawei.com>
2019-11-07 21:04:24 +0800

    clean

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -34,24 +34,24 @@ import (
 // Scheduler watches for new unscheduled pods for volcano. It attempts to find
 // nodes that they fit on and writes bindings back to the api server.
 type Scheduler struct {
-	cache          schedcache.Cache
-	config         *rest.Config
-	schedStConf    conf.SchedulerConf
-	schedulerConf  string
-	schedulePeriod time.Duration
+	cache              schedcache.Cache
+	config             *rest.Config
+	configFile         string
+	schedulePeriod     time.Duration
+	schedConfiguration *conf.SchedulerConf
 }
 
 // NewScheduler returns a scheduler
 func NewScheduler(
 	config *rest.Config,
 	schedulerName string,
-	conf string,
+	configFile string,
 	period time.Duration,
 	defaultQueue string,
 ) (*Scheduler, error) {
 	scheduler := &Scheduler{
 		config:         config,
-		schedulerConf:  conf,
+		configFile:     configFile,
 		cache:          schedcache.New(config, schedulerName, defaultQueue),
 		schedulePeriod: period,
 	}
@@ -62,7 +62,6 @@ func NewScheduler(
 // Run runs the Scheduler
 func (pc *Scheduler) Run(stopCh <-chan struct{}) {
 	var err error
-	var schedStConfig *conf.SchedulerConf
 
 	// Start cache for policy.
 	go pc.cache.Run(stopCh)
@@ -70,19 +69,18 @@ func (pc *Scheduler) Run(stopCh <-chan struct{}) {
 
 	// Load configuration of scheduler
 	schedConf := defaultSchedulerConf
-	if len(pc.schedulerConf) != 0 {
-		if schedConf, err = readSchedulerConf(pc.schedulerConf); err != nil {
+	if len(pc.configFile) != 0 {
+		if schedConf, err = readSchedulerConf(pc.configFile); err != nil {
 			glog.Errorf("Failed to read scheduler configuration '%s', using default configuration: %v",
-				pc.schedulerConf, err)
+				pc.configFile, err)
 			schedConf = defaultSchedulerConf
 		}
 	}
 
-	schedStConfig, err = loadSchedulerConf(schedConf)
+	pc.schedConfiguration, err = loadSchedulerConf(schedConf)
 	if err != nil {
 		panic(err)
 	}
-	pc.schedStConf = *schedStConfig
 
 	go wait.Until(pc.runOnce, pc.schedulePeriod, stopCh)
 }
@@ -93,11 +91,11 @@ func (pc *Scheduler) runOnce() {
 	defer glog.V(4).Infof("End scheduling ...")
 	defer metrics.UpdateE2eDuration(metrics.Duration(scheduleStartTime))
 
-	ssn := framework.OpenSession(pc.cache, pc.schedStConf)
+	ssn := framework.OpenSession(pc.cache, *pc.schedConfiguration)
 	defer framework.CloseSession(ssn)
 
-	if pc.schedStConf.Version == framework.SchedulerConfigVersion1 {
-		actionNames := strings.Split(pc.schedStConf.V1Conf.Actions, ",")
+	if pc.schedConfiguration.Version == framework.SchedulerConfigVersion1 {
+		actionNames := strings.Split(pc.schedConfiguration.V1Conf.Actions, ",")
 		for _, actionName := range actionNames {
 			if action, found := framework.GetAction(strings.TrimSpace(actionName)); found {
 				actionStartTime := time.Now()
@@ -105,9 +103,9 @@ func (pc *Scheduler) runOnce() {
 				metrics.UpdateActionDuration(action.Name(), metrics.Duration(actionStartTime))
 			}
 		}
-	} else if pc.schedStConf.Version == framework.SchedulerConfigVersion2 {
+	} else if pc.schedConfiguration.Version == framework.SchedulerConfigVersion2 {
 
-		for _, actionOpt := range pc.schedStConf.V2Conf.Actions {
+		for _, actionOpt := range pc.schedConfiguration.V2Conf.Actions {
 
 			if action, found := framework.GetAction(strings.TrimSpace(actionOpt.Name)); found {
 				actionStartTime := time.Now()
