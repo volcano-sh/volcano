98a3e2e4fa1263a21ab7763d69a331c8ce12d85c
Zhang Jinghui <zhangjinghui5@huawei.com>
2019-12-29 20:56:10 +0800

    considering best-effort pods when calculating ready task number

--- test/e2e/job_scheduling.go
+++ test/e2e/job_scheduling.go
@@ -158,6 +158,39 @@ var _ = Describe("Job E2E Test", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
+	It("Gang scheduling: Contains both best-effort pod and non-best-effort pod", func() {
+		context := initTestContext(options{})
+		defer cleanupTestContext(context)
+		rep := clusterSize(context, oneCPU)
+
+		if rep < 2 {
+			fmt.Println("Skip e2e test for insufficient resources.")
+			return
+		}
+
+		jobSpec := &jobSpec{
+			name:      "gang-both-best-effort-non-best-effort-pods",
+			namespace: "test",
+			tasks: []taskSpec{
+				{
+					img: defaultNginxImage,
+					req: oneCPU,
+					min: rep / 2,
+					rep: rep / 2,
+				},
+				{
+					img: defaultNginxImage,
+					min: rep - rep/2,
+					rep: rep - rep/2,
+				},
+			},
+		}
+
+		job := createJob(context, jobSpec)
+		err := waitJobReady(context, job)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
 	It("Preemption", func() {
 		context := initTestContext(options{
 			priorityClasses: map[string]int32{
