a466ae9803d61047266e2d003f3f4bd98a3d1737
Zhonghu Xu <xuzhonghu@huawei.com>
2020-01-15 14:43:57 +0800

    Revert "Gen install yaml without v1alpha queue and poggroup"

--- hack/generate-yaml.sh
+++ hack/generate-yaml.sh
@@ -72,6 +72,8 @@ ${HELM_BIN_DIR}/helm template ${VK_ROOT}/installer/helm/chart/volcano --namespac
       -x templates/bus_v1alpha1_command.yaml \
       -x templates/controllers.yaml \
       -x templates/scheduler.yaml \
+      -x templates/scheduling_v1alpha1_podgroup.yaml \
+      -x templates/scheduling_v1alpha1_queue.yaml \
       -x templates/scheduling_v1alpha2_podgroup.yaml \
       -x templates/scheduling_v1alpha2_queue.yaml \
       --notes >> ${DEPLOYMENT_FILE}
