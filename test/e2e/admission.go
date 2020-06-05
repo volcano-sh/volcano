b635fc66b9bbdd7aa8a9be3b3e86288f003c0948
Thor <1187526662@qq.com>
2020-06-05 15:26:02 +0800

    add pod admit e2e
    Signed-off-by: Thor <1187526662@qq.com>

--- test/e2e/admission.go
+++ test/e2e/admission.go
@@ -27,6 +27,7 @@ import (
 
 	"volcano.sh/volcano/pkg/apis/batch/v1alpha1"
 	schedulingv1beta1 "volcano.sh/volcano/pkg/apis/scheduling/v1beta1"
+	vcschedulingv1 "volcano.sh/volcano/pkg/apis/scheduling/v1beta1"
 )
 
 var _ = ginkgo.Describe("Job E2E Test: Test Admission service", func() {
@@ -168,6 +169,61 @@ var _ = ginkgo.Describe("Job E2E Test: Test Admission service", func() {
 			},
 			Spec: corev1.PodSpec{
 				Containers: createContainers(defaultNginxImage, "", "", oneCPU, oneCPU, 0),
+			},
+		}
+
+		_, err := ctx.kubeclient.CoreV1().Pods(ctx.namespace).Create(pod)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		err = waitPodPhase(ctx, pod, []corev1.PodPhase{corev1.PodRunning})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
+	ginkgo.It("Create volcano-scheduler pod", func() {
+		podName := "pod-volcano-scheduler"
+		ctx := initTestContext(options{})
+		defer cleanupTestContext(ctx)
+
+		pod := &corev1.Pod{
+			TypeMeta: v1.TypeMeta{
+				APIVersion: "v1",
+				Kind:       "Pod",
+			},
+			ObjectMeta: v1.ObjectMeta{
+				Namespace: ctx.namespace,
+				Name:      podName,
+			},
+			Spec: corev1.PodSpec{
+				Containers:    createContainers(defaultNginxImage, "", "", oneCPU, oneCPU, 0),
+				SchedulerName: "volcano",
+			},
+		}
+
+		_, err := ctx.kubeclient.CoreV1().Pods(ctx.namespace).Create(pod)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		err = waitPodPhase(ctx, pod, []corev1.PodPhase{corev1.PodRunning})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
+	ginkgo.It("Create volcano pod with volcano sheduler", func() {
+		podName := "volcano-pod"
+		ctx := initTestContext(options{})
+		defer cleanupTestContext(ctx)
+
+		pod := &corev1.Pod{
+			TypeMeta: v1.TypeMeta{
+				APIVersion: "v1",
+				Kind:       "Pod",
+			},
+			ObjectMeta: v1.ObjectMeta{
+				Namespace:   ctx.namespace,
+				Name:        podName,
+				Annotations: map[string]string{vcschedulingv1.KubeGroupNameAnnotationKey: "default"},
+			},
+			Spec: corev1.PodSpec{
+				Containers:    createContainers(defaultNginxImage, "", "", oneCPU, oneCPU, 0),
+				SchedulerName: "volcano",
 			},
 		}
 
