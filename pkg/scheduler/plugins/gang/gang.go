b20195335ab8045ec8621740152b1d086f5008ff
zionwu <zionwu3@gmail.com>
2020-09-23 15:50:48 +0800

    Remove creationTimestamp comparation in jobOrderFn of gang plugin
    Signed-off-by: zionwu <zionwu3@gmail.com>

--- pkg/scheduler/plugins/gang/gang.go
+++ pkg/scheduler/plugins/gang/gang.go
@@ -119,17 +119,6 @@ func (gp *gangPlugin) OnSessionOpen(ssn *framework.Session) {
 			return -1
 		}
 
-		if !lReady && !rReady {
-			if lv.CreationTimestamp.Equal(&rv.CreationTimestamp) {
-				if lv.UID < rv.UID {
-					return -1
-				}
-			} else if lv.CreationTimestamp.Before(&rv.CreationTimestamp) {
-				return -1
-			}
-			return 1
-		}
-
 		return 0
 	}
 
