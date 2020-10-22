9b6e06fd7eea512cad49d8a84585b9ec4fbaf12c
Thor-wl <1187526662@qq.com>
2020-10-22 20:49:20 +0800

    ci fix
    Signed-off-by: Thor-wl <1187526662@qq.com>

--- pkg/scheduler/api/job_info.go
+++ pkg/scheduler/api/job_info.go
@@ -144,7 +144,7 @@ type JobInfo struct {
 	CreationTimestamp metav1.Time
 	PodGroup          *PodGroup
 
-	ScheduleStartTimestamp  metav1.Time
+	ScheduleStartTimestamp metav1.Time
 }
 
 // NewJobInfo creates a new jobInfo for set of tasks
