b3054873b9fa5fe3ce2da9c72e485b98efbf6fa4
tangliang <1074461480@qq.com>
2020-11-04 20:26:40 +0800

    fix: improve func comments
    Signed-off-by: tangliang <1074461480@qq.com>

--- pkg/cli/util/util.go
+++ pkg/cli/util/util.go
@@ -201,7 +201,7 @@ func HumanDuration(d time.Duration) string {
 	return fmt.Sprintf("%dy", hours/24/365)
 }
 
-// GetInt32Ptr converts int32 to it's pointer
+// GetInt32Ptr converts int32 to int32 pointer
 func GetInt32Ptr(i int32) *int32 {
 	return &i
 }
