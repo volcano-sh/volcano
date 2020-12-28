8ce025290721844cd36a5660e603cf1c91c12c30
ChAnYaNG97 <790194334@qq.com>
2020-12-28 18:49:58 +0800

    go fmt update
    Signed-off-by: ChAnYaNG97 <790194334@qq.com>

--- cmd/scheduler/app/options/options.go
+++ cmd/scheduler/app/options/options.go
@@ -107,10 +107,10 @@ func (s *ServerOption) AddFlags(fs *pflag.FlagSet) {
 
 	// The percentage of nodes that would be scored in each scheduling cycle; if <= 0, an adpative percentage will be calcuated
 	fs.Int32Var(&s.PercentageOfNodesToFind, "percentage-nodes-to-find", defaultPercentageOfNodesToFind, "The percentage of nodes to find and score, if <=0 will be calcuated based on the cluster size")
-  
+
 	// The node selector
 	fs.StringVar(&s.NodeSelector, "node-selector", "", "The node selector in the form of \"<key>:<value>\"")
-  
+
 	fs.StringVar(&s.PluginsDir, "plugins-dir", defaultPluginsDir, "vc-scheduler will load custom plugins which are in this directory")
 }
 
