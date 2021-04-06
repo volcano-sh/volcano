849042a8f4a459176d37b41cf6398ce49fcadcc6
ZhengYu, Xu <zen-xu@outlook.com>
2021-04-06 15:19:31 +0800

    chore: reposition `Job` kubebuilder markers
    Signed-off-by: ZhengYu, Xu <zen-xu@outlook.com>

--- pkg/apis/batch/v1alpha1/job.go
+++ pkg/apis/batch/v1alpha1/job.go
@@ -1,17 +1,17 @@
 /*
-Copyright 2018 The Volcano Authors.
+ Copyright 2021 The Volcano Authors.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 */
 
 package v1alpha1
@@ -27,8 +27,6 @@ import (
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:path=jobs,shortName=vcjob;vj
 // +kubebuilder:subresource:status
-
-// Job defines the volcano job.
 // +kubebuilder:printcolumn:name="Pending",type=integer,JSONPath=`.status.pending`
 // +kubebuilder:printcolumn:name="Running",type=integer,JSONPath=`.status.running`
 // +kubebuilder:printcolumn:name="Succeeded",type=integer,JSONPath=`.status.succeeded`
@@ -39,6 +37,8 @@ import (
 // +kubebuilder:printcolumn:name="MinAvailable",type=integer,JSONPath=`.status.minAvailable`
 // +kubebuilder:printcolumn:name="State",type=string,JSONPath=`.status.state.phase`
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
+
+// Job defines the volcano job.
 type Job struct {
 	metav1.TypeMeta `json:",inline"`
 
