a17e572660a3ad22a3d2b84163ae7d06da708f7c
william-wang <wang.platform@gmail.com>
2022-03-12 13:55:03 +0800

    fix the error message
    Signed-off-by: william-wang <wang.platform@gmail.com>

--- pkg/webhooks/admission/jobs/validate/admit_job_test.go
+++ pkg/webhooks/admission/jobs/validate/admit_job_test.go
@@ -457,7 +457,7 @@ func TestValidateJobCreate(t *testing.T) {
 				},
 			},
 			reviewResponse: admissionv1.AdmissionResponse{Allowed: false},
-			ret:            "[a lowercase RFC 1123 label must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character (e.g. 'my-name',  or '123-abc', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?')];",
+			ret:            "[a DNS-1123 label must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character (e.g. 'my-name',  or '123-abc', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?')];",
 			ExpectErr:      true,
 		},
 		// Policy Event with exit code
