7d40ce984a9f01ec41b185246892cbd45a32a027
hwdef <hwdefcom@outlook.com>
2022-05-05 15:53:34 +0800

    add mpi plugin
    Signed-off-by: hwdef <hwdefcom@outlook.com>

---
+++ pkg/webhooks/admission/jobs/plugins/mpi/mpi_test.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2022 The Volcano Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package mpi
+
+import (
+	"testing"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"volcano.sh/apis/pkg/apis/batch/v1alpha1"
+	controllerMpi "volcano.sh/volcano/pkg/controllers/job/plugins/distributed-framework/mpi"
+)
+
+func TestMpi(t *testing.T) {
+	workerName := "fakeWorker"
+	masterName := "fakeMaster"
+	plugins := make(map[string][]string)
+	plugins[controllerMpi.MpiPluginName] = []string{"--master=" + masterName, "--worker=" + workerName}
+	testcases := []struct {
+		Name string
+		Job  *v1alpha1.Job
+	}{
+		{
+			Name: "add dependsOn",
+			Job: &v1alpha1.Job{
+				ObjectMeta: metav1.ObjectMeta{Name: "test-mpi-0"},
+				Spec: v1alpha1.JobSpec{
+					Plugins: plugins,
+					Tasks: []v1alpha1.TaskSpec{
+						{
+							Name:     masterName,
+							Replicas: 1,
+							Template: v1.PodTemplateSpec{},
+						},
+						{
+							Name:     workerName,
+							Replicas: 2,
+							Template: v1.PodTemplateSpec{},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for index, testcase := range testcases {
+		t.Run(testcase.Name, func(t *testing.T) {
+			AddDependsOn(testcase.Job)
+			if testcase.Job.Spec.Tasks[0].DependsOn == nil || len(testcase.Job.Spec.Tasks[0].DependsOn.Name) == 0 {
+				t.Errorf("Case %d (%s): no dependencies added ", index, testcase.Name)
+				return
+			}
+			if testcase.Job.Spec.Tasks[0].DependsOn.Name[0] != workerName {
+				t.Errorf("Case %d (%s): Dependency add error expect %s, but got %s", index, testcase.Name, workerName, testcase.Job.Spec.Tasks[0].DependsOn.Name[0])
+			}
+		})
+	}
+}
---
+++ test/e2e/jobseq/mpi_plugin.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2022 The Volcano Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package jobseq
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	vcbatch "volcano.sh/apis/pkg/apis/batch/v1alpha1"
+	vcbus "volcano.sh/apis/pkg/apis/bus/v1alpha1"
+	e2eutil "volcano.sh/volcano/test/e2e/util"
+)
+
+var _ = Describe("MPI Plugin E2E Test", func() {
+	It("will run and complete finally", func() {
+		context := e2eutil.InitTestContext(e2eutil.Options{})
+		defer e2eutil.CleanupTestContext(context)
+
+		slot := e2eutil.OneCPU
+
+		spec := &e2eutil.JobSpec{
+			Name: "mpi",
+			Min:  1,
+			Policies: []vcbatch.LifecyclePolicy{
+				{
+					Action: vcbus.CompleteJobAction,
+					Event:  vcbus.TaskCompletedEvent,
+				},
+			},
+			Plugins: map[string][]string{
+				"mpi": {"--master=mpimaster", "--worker=mpiworker", "--port=22"},
+			},
+			Tasks: []e2eutil.TaskSpec{
+				{
+					Name:       "mpimaster",
+					Img:        e2eutil.DefaultMPIImage,
+					Req:        slot,
+					Min:        1,
+					Rep:        1,
+					WorkingDir: "/home",
+					// Need sometime waiting for worker node ready
+					Command: `mkdir -p /var/run/sshd; /usr/sbin/sshd;
+mpiexec --allow-run-as-root --host ${MPI_HOST} -np 2 mpi_hello_world > /home/re`,
+				},
+				{
+					Name:       "mpiworker",
+					Img:        e2eutil.DefaultMPIImage,
+					Req:        slot,
+					Min:        2,
+					Rep:        2,
+					WorkingDir: "/home",
+					Command:    "mkdir -p /var/run/sshd; /usr/sbin/sshd -D;",
+				},
+			},
+		}
+
+		job := e2eutil.CreateJob(context, spec)
+		err := e2eutil.WaitJobPhases(context, job, []vcbatch.JobPhase{
+			vcbatch.Pending, vcbatch.Running, vcbatch.Completing, vcbatch.Completed})
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
