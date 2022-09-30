61d0b7221d2c9068772a7edbc3a3700f43bf3a2a
wulixuan <wulixuan@bilibili.com>
2022-09-30 11:21:23 +0800

    scheduler support ephemeral-storage resources
    Signed-off-by: wulixuan <931845457@qq.com>

--- pkg/scheduler/api/resource_info.go
+++ pkg/scheduler/api/resource_info.go
@@ -76,6 +76,8 @@ func NewResource(rl v1.ResourceList) *Resource {
 			r.Memory += float64(rQuant.Value())
 		case v1.ResourcePods:
 			r.MaxTaskNum += int(rQuant.Value())
+		case v1.ResourceEphemeralStorage:
+			r.AddScalar(rName, float64(rQuant.MilliValue()))
 		default:
 			if IsCountQuota(rName) {
 				continue
