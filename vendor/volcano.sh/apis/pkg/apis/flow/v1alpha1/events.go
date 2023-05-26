b0fd63618394e3905f8c811be5a2853b55a0a157
晓杰 <2561589453@qq.com>
2023-05-26 19:57:54 +0800

    remove vendor
    Signed-off-by: 晓杰 <2561589453@qq.com>

--- vendor/volcano.sh/apis/pkg/apis/flow/v1alpha1/events.go
+++
@@ -1,9 +0,0 @@
-package v1alpha1
-
-// Event represent the phase of JobFlow
-type Event string
-
-const (
-	// OutOfSyncEvent is triggered if JobFlow is updated(add/update/delete)
-	OutOfSyncEvent Event = "OutOfSync"
-)
