362dc00a22facb25571a41bab2c9d6cf84890f83
googs1025 <googs1025@gmail.com>
2024-04-14 21:13:47 +0800

    add ut for pkg/scheduler/plugins/util/k8s package
    Signed-off-by: googs1025 <googs1025@gmail.com>
    (cherry picked from commit 224c8d48b30a0069b8fea4d23cf588b9aa2092b9)
    Signed-off-by: Monokaix <changxuzheng@huawei.com>

---
+++ pkg/scheduler/plugins/util/nodelock/nodelock_test.go
@@ -0,0 +1,61 @@
+package nodelock
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestLockNode(t *testing.T) {
+	var (
+		nodeName = "test-node"
+		lockName = "test-node-lock"
+	)
+
+	tests := []struct {
+		name      string
+		expectErr error
+		node      *v1.Node
+	}{
+		{
+			name:      "lock node success",
+			expectErr: nil,
+			node: &v1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: nodeName,
+					Annotations: map[string]string{
+						lockName: time.Now().Add(-time.Minute * 20).Format(time.RFC3339),
+					},
+				},
+			},
+		},
+		{
+			name:      "lock node failed",
+			expectErr: fmt.Errorf("node %s has been locked within 5 minutes", nodeName),
+			node: &v1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: nodeName,
+					Annotations: map[string]string{
+						lockName: time.Now().Add(time.Minute * 20).Format(time.RFC3339),
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fakeClient := fake.NewSimpleClientset(tc.node)
+			_ = UseClient(fakeClient)
+			gotErr := LockNode(nodeName, lockName)
+			if !reflect.DeepEqual(tc.expectErr, gotErr) {
+				t.Errorf("LockNode error: (+got: %T/-want: %T)", gotErr, tc.expectErr)
+			}
+		})
+	}
+}
