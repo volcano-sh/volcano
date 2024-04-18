173002222410d56431d02646fc314b13208ddeec
googs1025 <googs1025@gmail.com>
2024-04-18 22:19:17 +0800

    refecotr: nodelock
    Signed-off-by: googs1025 <googs1025@gmail.com>
    (cherry picked from commit 1a7c18a09283f6daf73e483e9ffa00780d74d008)
    Signed-off-by: Monokaix <changxuzheng@huawei.com>

--- pkg/scheduler/plugins/util/k8s/snapshot_test.go
+++ pkg/scheduler/plugins/util/k8s/snapshot_test.go
@@ -6,71 +6,22 @@ import (
 	"testing"
 
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
+
+	"volcano.sh/volcano/pkg/scheduler/api"
+	"volcano.sh/volcano/pkg/scheduler/util"
 )
 
 func TestSnapshot(t *testing.T) {
 	var (
 		nodeName = "test-node"
-		pod1     = &v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: "node_info_cache_test",
-				Name:      "test-1",
-				UID:       types.UID("test-1"),
-			},
-			Spec: v1.PodSpec{
-				Containers: []v1.Container{
-					{
-						Resources: v1.ResourceRequirements{
-							Requests: v1.ResourceList{
-								v1.ResourceCPU:    resource.MustParse("100m"),
-								v1.ResourceMemory: resource.MustParse("500"),
-							},
-						},
-						Ports: []v1.ContainerPort{
-							{
-								HostIP:   "127.0.0.1",
-								HostPort: 80,
-								Protocol: "TCP",
-							},
-						},
-					},
-				},
-				NodeName: nodeName,
-			},
-		}
-		pod2 = &v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: "node_info_cache_test",
-				Name:      "test-2",
-				UID:       types.UID("test-2"),
-				Labels:    map[string]string{"test": "test"},
-			},
-			Spec: v1.PodSpec{
-				Containers: []v1.Container{
-					{
-						Resources: v1.ResourceRequirements{
-							Requests: v1.ResourceList{
-								v1.ResourceCPU:    resource.MustParse("200m"),
-								v1.ResourceMemory: resource.MustParse("1Ki"),
-							},
-						},
-						Ports: []v1.ContainerPort{
-							{
-								HostIP:   "127.0.0.1",
-								HostPort: 8080,
-								Protocol: "TCP",
-							},
-						},
-					},
-				},
-				NodeName: nodeName,
-			},
-		}
+		pod1     = util.BuildPod("node_info_cache_test", "test-1", nodeName,
+			v1.PodRunning, api.BuildResourceList("200m", "1Ki"), "pg2",
+			make(map[string]string), make(map[string]string))
+		pod2 = util.BuildPod("node_info_cache_test", "test-2", nodeName,
+			v1.PodRunning, api.BuildResourceList("200m", "1Ki"), "pg2",
+			map[string]string{"test": "test"}, make(map[string]string))
 	)
 
 	tests := []struct {
@@ -87,14 +38,8 @@ func TestSnapshot(t *testing.T) {
 				NonZeroRequested: &framework.Resource{},
 				Allocatable:      &framework.Resource{},
 				Generation:       2,
-				UsedPorts: framework.HostPortInfo{
-					"127.0.0.1": map[framework.ProtocolPort]struct{}{
-						{Protocol: "TCP", Port: 80}:   {},
-						{Protocol: "TCP", Port: 8080}: {},
-					},
-				},
-				ImageStates:  map[string]*framework.ImageStateSummary{},
-				PVCRefCounts: map[string]int{},
+				ImageStates:      map[string]*framework.ImageStateSummary{},
+				PVCRefCounts:     map[string]int{},
 				Pods: []*framework.PodInfo{
 					{
 						Pod: pod1,
@@ -125,14 +70,8 @@ func TestSnapshot(t *testing.T) {
 				NonZeroRequested: &framework.Resource{},
 				Allocatable:      &framework.Resource{},
 				Generation:       2,
-				UsedPorts: framework.HostPortInfo{
-					"127.0.0.1": map[framework.ProtocolPort]struct{}{
-						{Protocol: "TCP", Port: 80}:   {},
-						{Protocol: "TCP", Port: 8080}: {},
-					},
-				},
-				ImageStates:  map[string]*framework.ImageStateSummary{},
-				PVCRefCounts: map[string]int{},
+				ImageStates:      map[string]*framework.ImageStateSummary{},
+				PVCRefCounts:     map[string]int{},
 				Pods: []*framework.PodInfo{
 					{
 						Pod: pod1,
@@ -168,45 +107,40 @@ func TestSnapshot(t *testing.T) {
 			snapshot := NewSnapshot(tc.nodeInfoMap)
 			nodeInfoList, err := snapshot.List()
 			if !reflect.DeepEqual(nodeInfoList, tc.expectedNodeInfos) || err != nil {
-				t.Errorf("unexpected list nodeInfos value (+got: %s/-want: %s), err: %s", tc.expectedNodeInfos, nodeInfoList, err)
+				t.Fatalf("unexpected list nodeInfos value (+got: %s/-want: %s), err: %s", tc.expectedNodeInfos, nodeInfoList, err)
 			}
 
 			_, err = snapshot.Get(nodeName)
 			if !reflect.DeepEqual(tc.expectErr, err) {
-				t.Errorf("unexpected get nodeInfos by nodeName value (+got: %T/-want: %T)", err, tc.expectErr)
+				t.Fatalf("unexpected get nodeInfos by nodeName value (+got: %T/-want: %T)", err, tc.expectErr)
 			}
 
 			nodeInfoList, err = snapshot.HavePodsWithAffinityList()
 			if !reflect.DeepEqual(tc.expectedNodeInfos, nodeInfoList) || err != nil {
-				t.Errorf("unexpected list HavePodsWithAffinity nodeInfos value (+got: %s/-want: %s), err: %s", nodeInfoList, tc.expectedNodeInfos, err)
+				t.Fatalf("unexpected list HavePodsWithAffinity nodeInfos value (+got: %s/-want: %s), err: %s", nodeInfoList, tc.expectedNodeInfos, err)
 			}
 
 			nodeInfoList, err = snapshot.HavePodsWithRequiredAntiAffinityList()
 			if !reflect.DeepEqual(tc.expectedNodeInfos, nodeInfoList) || err != nil {
-				t.Errorf("unexpected list PodsWithRequiredAntiAffinity nodeInfos value (+got: %s/-want: %s), err: %s", nodeInfoList, tc.expectedNodeInfos, err)
+				t.Fatalf("unexpected list PodsWithRequiredAntiAffinity nodeInfos value (+got: %s/-want: %s), err: %s", nodeInfoList, tc.expectedNodeInfos, err)
 			}
 
 			sel, err := labels.Parse("test==test")
 			pods, err := snapshot.Pods().List(sel)
 			if !reflect.DeepEqual(tc.expectedPods, pods) || err != nil {
-				t.Errorf("unexpected list pods value (+got: %s/-want: %s), err: %s", pods, tc.expectedNodeInfos, err)
+				t.Fatalf("unexpected list pods value (+got: %s/-want: %s), err: %s", pods, tc.expectedNodeInfos, err)
 			}
 
 			pods, err = snapshot.Pods().FilteredList(func(pod *v1.Pod) bool {
 				return true
 			}, sel)
 			if !reflect.DeepEqual(tc.expectedPods, pods) || err != nil {
-				t.Errorf("unexpected list filtered pods value (+got: %s/-want: %s), err: %s", pods, tc.expectedPods, err)
+				t.Fatalf("unexpected list filtered pods value (+got: %s/-want: %s), err: %s", pods, tc.expectedPods, err)
 			}
 
 			nodeInfos, err := snapshot.NodeInfos().List()
 			if !reflect.DeepEqual(tc.expectedNodeInfos, nodeInfos) || err != nil {
-				t.Errorf("unexpected list nodeInfos value (+got: %s/-want: %s), err: %s", nodeInfos, tc.expectedNodeInfos, err)
-			}
-
-			getBool := snapshot.StorageInfos().IsPVCUsedByPods("test")
-			if !reflect.DeepEqual(false, getBool) {
-				t.Errorf("unexpected get StorageInfos PVCUsed value (+got: %v/-want: %v)", false, getBool)
+				t.Fatalf("unexpected list nodeInfos value (+got: %s/-want: %s), err: %s", nodeInfos, tc.expectedNodeInfos, err)
 			}
 		})
 	}
