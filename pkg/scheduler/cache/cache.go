c04ddac61ee96d59056ed0dae1aafabf9eff7309
Wenbo Zhang <zhangwenbo40@huawei.com>
2024-04-23 20:30:46 +0800

    fix comments
    Signed-off-by: Wenbo Zhang <zhangwenbo40@huawei.com>

--- pkg/scheduler/cache/cache.go
+++ pkg/scheduler/cache/cache.go
@@ -1104,6 +1104,7 @@ func (sc *SchedulerCache) processResyncTask() {
 	taskKey, ok := obj.(string)
 	if !ok {
 		klog.Errorf("Failed to convert %v to string.", obj)
+		sc.errTasks.Forget(obj)
 		return
 	}
 
@@ -1120,7 +1121,7 @@ func (sc *SchedulerCache) processResyncTask() {
 		sc.resyncTask(task)
 		reSynced = true
 	} else {
-		klog.V(4).Infof("sync task <%s/%s> success", task.Namespace, task.Name)
+		klog.V(4).Infof("Successfully synced task <%s/%s>", task.Namespace, task.Name)
 		sc.errTasks.Forget(obj)
 	}
 
