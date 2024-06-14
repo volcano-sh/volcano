a47b7ec4f9e407d86db17a0d125ebba72cdd02b4
wangyysde <net_use@bzhy.com>
2024-06-14 18:14:54 +0800

    avoid unnecessary API update and re-enqueue action
    Signed-off-by: wangyysde <net_use@bzhy.com>

--- pkg/controllers/job/job_controller_actions.go
+++ pkg/controllers/job/job_controller_actions.go
@@ -476,11 +476,6 @@ func (cc *jobcontroller) syncJob(jobInfo *apis.JobInfo, updateStatus state.Updat
 	if reflect.DeepEqual(job.Status, newStatus) {
 		klog.V(3).Infof("Job <%s/%s> has not updated for no changing", job.Namespace, job.Name)
 		return nil
-	} else {
-		job.Status = newStatus
-		job.Status.State.LastTransitionTime = metav1.Now()
-		jobCondition = newCondition(job.Status.State.Phase, &job.Status.State.LastTransitionTime)
-		job.Status.Conditions = append(job.Status.Conditions, jobCondition)
 	}
 	newJob, err := cc.vcClient.BatchV1alpha1().Jobs(job.Namespace).UpdateStatus(context.TODO(), job, metav1.UpdateOptions{})
 	if err != nil {
