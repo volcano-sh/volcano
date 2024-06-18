fa6d8ec6f9c1c2b620f255928a04a868e05010d7
chuanX <cxu1995@hotmail.com>
2024-06-18 13:18:07 +0800

    Delete unused variables.
    Signed-off-by: chuanX <cxu1995@hotmail.com>

--- pkg/scheduler/plugins/sla/sla.go
+++ pkg/scheduler/plugins/sla/sla.go
@@ -73,7 +73,7 @@ tiers:
     arguments:
     sla-waiting-time: 1h2m3s4ms5µs6ns
 
-Meanwhile, use can give individual job waiting time settings for one job via job annotations:
+Meanwhile, user can give individual job waiting time settings for one job via job annotations:
 apiVersion: batch.volcano.sh/v1alpha1
 kind: Job
 metadata:
