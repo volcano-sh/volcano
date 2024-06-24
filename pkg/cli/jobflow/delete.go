73784df0ef99da1992a9e311abebc6cdabc3eea0
googs1025 <googs1025@gmail.com>
2024-06-24 08:42:48 +0800

    feat: add vcctl jobflow command
    Signed-off-by: googs1025 <googs1025@gmail.com>
    (cherry picked from commit 93b6e7121da2cbc5c35dedaa18e819b3f9d54bb0)
    Signed-off-by: Monokaix <changxuzheng@huawei.com>

---
+++ pkg/cli/jobflow/delete.go
@@ -0,0 +1,104 @@
+package jobflow
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
+
+	flowv1alpha1 "volcano.sh/apis/pkg/apis/flow/v1alpha1"
+	"volcano.sh/apis/pkg/client/clientset/versioned"
+	"volcano.sh/volcano/pkg/cli/util"
+)
+
+type deleteFlags struct {
+	util.CommonFlags
+
+	// Name is name of jobflow
+	Name string
+	// Namespace is namespace of jobflow
+	Namespace string
+	// FilePath is the file path of jobflow
+	FilePath string
+}
+
+var deleteJobFlowFlags = &deleteFlags{}
+
+// InitDeleteFlags is used to init all flags during jobflow deleting.
+func InitDeleteFlags(cmd *cobra.Command) {
+	util.InitFlags(cmd, &deleteJobFlowFlags.CommonFlags)
+	cmd.Flags().StringVarP(&deleteJobFlowFlags.Name, "name", "N", "", "the name of jobflow")
+	cmd.Flags().StringVarP(&deleteJobFlowFlags.Namespace, "namespace", "n", "default", "the namespace of jobflow")
+	cmd.Flags().StringVarP(&deleteJobFlowFlags.FilePath, "file", "f", "", "the path to the YAML file containing the jobflow")
+}
+
+// DeleteJobFlow is used to delete a jobflow.
+func DeleteJobFlow(ctx context.Context) error {
+	config, err := util.BuildConfig(deleteJobFlowFlags.Master, deleteJobFlowFlags.Kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	jobFlowClient := versioned.NewForConfigOrDie(config)
+	if err != nil {
+		return err
+	}
+
+	if deleteJobFlowFlags.FilePath != "" {
+		yamlData, err := os.ReadFile(deleteJobFlowFlags.FilePath)
+		if err != nil {
+			return err
+		}
+
+		yamlDocs := strings.Split(string(yamlData), "---")
+
+		deletedCount := 0
+		for _, doc := range yamlDocs {
+			doc = strings.TrimSpace(doc)
+			if doc == "" {
+				continue
+			}
+
+			jobFlow := &flowv1alpha1.JobFlow{}
+			if err := yaml.Unmarshal([]byte(doc), jobFlow); err != nil {
+				return err
+			}
+
+			if jobFlow.Namespace == "" {
+				jobFlow.Namespace = "default"
+			}
+
+			err := jobFlowClient.FlowV1alpha1().JobFlows(jobFlow.Namespace).Delete(ctx, jobFlow.Name, metav1.DeleteOptions{})
+			if err == nil {
+				fmt.Printf("Deleted JobFlow: %s/%s\n", jobFlow.Namespace, jobFlow.Name)
+				deletedCount++
+			} else {
+				fmt.Printf("Failed to delete JobFlow: %v\n", err)
+			}
+		}
+		return nil
+	}
+
+	if deleteJobFlowFlags.Name == "" {
+		return fmt.Errorf("jobflow name must be specified")
+	}
+
+	jobFlow, err := jobFlowClient.FlowV1alpha1().JobFlows(deleteJobFlowFlags.Namespace).Get(ctx, deleteJobFlowFlags.Name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	err = jobFlowClient.FlowV1alpha1().JobFlows(jobFlow.Namespace).Delete(ctx, jobFlow.Name, metav1.DeleteOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Deleted JobFlow: %s/%s\n", jobFlow.Namespace, jobFlow.Name)
+
+	return nil
+}
---
+++ pkg/cli/jobflow/get.go
@@ -0,0 +1,96 @@
+package jobflow
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"volcano.sh/apis/pkg/apis/flow/v1alpha1"
+	"volcano.sh/apis/pkg/client/clientset/versioned"
+	"volcano.sh/volcano/pkg/cli/util"
+)
+
+type getFlags struct {
+	util.CommonFlags
+	// Name of the jobflow
+	Name string
+	// Namespace of the jobflow
+	Namespace string
+}
+
+var getJobFlowFlags = &getFlags{}
+
+// InitGetFlags is used to init all flags.
+func InitGetFlags(cmd *cobra.Command) {
+	util.InitFlags(cmd, &getJobFlowFlags.CommonFlags)
+	cmd.Flags().StringVarP(&getJobFlowFlags.Name, "name", "N", "", "the name of jobflow")
+	cmd.Flags().StringVarP(&getJobFlowFlags.Namespace, "namespace", "n", "default", "the namespace of jobflow")
+}
+
+// GetJobFlow gets a jobflow.
+func GetJobFlow(ctx context.Context) error {
+	config, err := util.BuildConfig(getJobFlowFlags.Master, getJobFlowFlags.Kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	if getJobFlowFlags.Name == "" {
+		err := fmt.Errorf("name is mandatory to get the particular jobflow details")
+		return err
+	}
+
+	jobFlowClient := versioned.NewForConfigOrDie(config)
+	jobFlow, err := jobFlowClient.FlowV1alpha1().JobFlows(getJobFlowFlags.Namespace).Get(ctx, getJobFlowFlags.Name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	PrintJobFlow(jobFlow, os.Stdout)
+
+	return nil
+}
+
+// PrintJobFlow prints the jobflow details.
+func PrintJobFlow(jobFlow *v1alpha1.JobFlow, writer io.Writer) {
+	maxNameLen := len(Name)
+	maxNamespaceLen := len(Namespace)
+	maxPhaseLen := len(Phase)
+	maxAgeLen := len(Age)
+	if len(jobFlow.Name) > maxNameLen {
+		maxNameLen = len(jobFlow.Name)
+	}
+	if len(jobFlow.Namespace) > maxNamespaceLen {
+		maxNamespaceLen = len(jobFlow.Namespace)
+	}
+	if len(jobFlow.Status.State.Phase) > maxPhaseLen {
+		maxPhaseLen = len(jobFlow.Status.State.Phase)
+	}
+	age := translateTimestampSince(jobFlow.CreationTimestamp)
+	if len(age) > maxAgeLen {
+		maxAgeLen = len(age)
+	}
+
+	columnSpacing := 4
+	maxNameLen += columnSpacing
+	maxNamespaceLen += columnSpacing
+	maxPhaseLen += columnSpacing
+	maxAgeLen += columnSpacing
+	// Find the max length of the name, namespace.
+	formatStr := fmt.Sprintf("%%-%ds%%-%ds%%-%ds%%-%ds\n", maxNameLen, maxNamespaceLen, maxPhaseLen, maxAgeLen)
+
+	// Print the header.
+	_, err := fmt.Fprintf(writer, formatStr, Name, Namespace, Phase, Age)
+	if err != nil {
+		fmt.Printf("Failed to print JobFlow command result: %s.\n", err)
+	}
+	// Print the separator.
+	_, err = fmt.Fprintf(writer, formatStr, jobFlow.Name, jobFlow.Namespace, jobFlow.Status.State.Phase, age)
+	if err != nil {
+		fmt.Printf("Failed to print JobFlow command result: %s.\n", err)
+	}
+}
