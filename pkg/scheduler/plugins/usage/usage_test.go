7b516dba8330a9e58c4b98f27bda9f14a85b17d5
lowang-bh <lhui_wang@163.com>
2024-06-29 15:01:07 +0800

    refact testcase in usage plugin with uthelper framework
    Signed-off-by: lowang-bh <lhui_wang@163.com>
    
    Signed-off-by: lowang-bh <lhui_wang@163.com>
    (cherry picked from commit fa3bcb6f87b30bc781087b09038ccb75fa255202)
    Signed-off-by: Monokaix <changxuzheng@huawei.com>

--- pkg/scheduler/plugins/usage/usage_test.go
+++ pkg/scheduler/plugins/usage/usage_test.go
@@ -19,21 +19,18 @@ package usage
 import (
 	"fmt"
 	"math"
-	"reflect"
+	"strings"
 	"testing"
 	"time"
 
-	"github.com/agiledragon/gomonkey/v2"
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/record"
 
 	schedulingv1 "volcano.sh/apis/pkg/apis/scheduling/v1beta1"
 	"volcano.sh/volcano/pkg/scheduler/api"
-	"volcano.sh/volcano/pkg/scheduler/cache"
 	"volcano.sh/volcano/pkg/scheduler/conf"
 	"volcano.sh/volcano/pkg/scheduler/framework"
 	"volcano.sh/volcano/pkg/scheduler/metrics/source"
+	"volcano.sh/volcano/pkg/scheduler/uthelper"
 	"volcano.sh/volcano/pkg/scheduler/util"
 )
 
@@ -63,14 +60,7 @@ func updateNodeUsage(nodesInfo map[string]*api.NodeInfo, nodesUsage map[string]*
 }
 
 func TestUsage_predicateFn(t *testing.T) {
-	var tmp *cache.SchedulerCache
-	patchUpdateQueueStatus := gomonkey.ApplyMethod(reflect.TypeOf(tmp), "UpdateQueueStatus", func(scCache *cache.SchedulerCache, queue *api.QueueInfo) error {
-		return nil
-	})
-	defer patchUpdateQueueStatus.Reset()
-
-	framework.RegisterPluginBuilder(PluginName, New)
-	defer framework.CleanupPluginBuilders()
+	plugins := map[string]framework.PluginBuilder{PluginName: New}
 
 	p1 := util.BuildPod("c1", "p1", "", v1.PodPending, api.BuildResourceList("1", "1Gi"), "pg1", make(map[string]string), make(map[string]string))
 	p2 := util.BuildPod("c1", "p2", "", v1.PodPending, api.BuildResourceList("1", "1Gi"), "pg1", make(map[string]string), make(map[string]string))
@@ -101,47 +91,23 @@ func TestUsage_predicateFn(t *testing.T) {
 	// The node can schedule pods.
 	nodesUsage[n5.Name] = buildNodeUsage(map[string]float64{source.NODE_METRICS_PERIOD: 90}, map[string]float64{source.NODE_METRICS_PERIOD: 81}, time.Time{})
 
-	pg1 := &schedulingv1.PodGroup{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pg1",
-			Namespace: "c1",
-		},
-		Spec: schedulingv1.PodGroupSpec{
-			Queue: "q1",
-		},
-	}
-	queue1 := &schedulingv1.Queue{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "q1",
-		},
-		Spec: schedulingv1.QueueSpec{
-			Weight: 1,
-		},
-	}
+	pg1 := util.BuildPodGroup("pg1", "c1", "q1", 0, nil, "")
+
+	queue1 := util.BuildQueue("q1", 1, nil)
 
 	tests := []struct {
-		name          string
-		podGroups     []*schedulingv1.PodGroup
-		queues        []*schedulingv1.Queue
-		pods          []*v1.Pod
-		nodes         []*v1.Node
+		uthelper.TestCommonStruct
 		nodesUsageMap map[string]*api.NodeUsage
 		arguments     framework.Arguments
 		expected      predicateResult
 	}{
 		{
-			name: "The node cannot be scheduled, because of the CPU load of the node exceeds the upper limit.",
-			podGroups: []*schedulingv1.PodGroup{
-				pg1,
-			},
-			queues: []*schedulingv1.Queue{
-				queue1,
-			},
-			pods: []*v1.Pod{
-				p1, p2,
-			},
-			nodes: []*v1.Node{
-				n1,
+			TestCommonStruct: uthelper.TestCommonStruct{
+				Name:      "The node cannot be scheduled, because of the CPU load of the node exceeds the upper limit.",
+				PodGroups: []*schedulingv1.PodGroup{pg1},
+				Queues:    []*schedulingv1.Queue{queue1},
+				Pods:      []*v1.Pod{p1, p2},
+				Nodes:     []*v1.Node{n1},
 			},
 			nodesUsageMap: nodesUsage,
 			arguments: framework.Arguments{
@@ -164,18 +130,12 @@ func TestUsage_predicateFn(t *testing.T) {
 			},
 		},
 		{
-			name: "The node cannot be scheduled, because of the memory load of the node exceeds the upper limit.",
-			podGroups: []*schedulingv1.PodGroup{
-				pg1,
-			},
-			queues: []*schedulingv1.Queue{
-				queue1,
-			},
-			pods: []*v1.Pod{
-				p1, p2,
-			},
-			nodes: []*v1.Node{
-				n2,
+			TestCommonStruct: uthelper.TestCommonStruct{
+				Name:      "The node cannot be scheduled, because of the memory load of the node exceeds the upper limit.",
+				PodGroups: []*schedulingv1.PodGroup{pg1},
+				Queues:    []*schedulingv1.Queue{queue1},
+				Pods:      []*v1.Pod{p1, p2},
+				Nodes:     []*v1.Node{n2},
 			},
 			nodesUsageMap: nodesUsage,
 			arguments: framework.Arguments{
@@ -198,18 +158,12 @@ func TestUsage_predicateFn(t *testing.T) {
 			},
 		},
 		{
-			name: "The node can be scheduled, because of the CPU usage and memory usage do not exceed the upper limit.",
-			podGroups: []*schedulingv1.PodGroup{
-				pg1,
-			},
-			queues: []*schedulingv1.Queue{
-				queue1,
-			},
-			pods: []*v1.Pod{
-				p1, p2,
-			},
-			nodes: []*v1.Node{
-				n3,
+			TestCommonStruct: uthelper.TestCommonStruct{
+				Name:      "The node can be scheduled, because of the CPU usage and memory usage do not exceed the upper limit.",
+				PodGroups: []*schedulingv1.PodGroup{pg1},
+				Queues:    []*schedulingv1.Queue{queue1},
+				Pods:      []*v1.Pod{p1, p2},
+				Nodes:     []*v1.Node{n3},
 			},
 			nodesUsageMap: nodesUsage,
 			arguments: framework.Arguments{
@@ -232,18 +186,12 @@ func TestUsage_predicateFn(t *testing.T) {
 			},
 		},
 		{
-			name: "The node can be scheduled, because of the metrics are not updated in the latest 5 minutes, and the usage function is invalid.",
-			podGroups: []*schedulingv1.PodGroup{
-				pg1,
-			},
-			queues: []*schedulingv1.Queue{
-				queue1,
-			},
-			pods: []*v1.Pod{
-				p1, p2,
-			},
-			nodes: []*v1.Node{
-				n4,
+			TestCommonStruct: uthelper.TestCommonStruct{
+				Name:      "The node can be scheduled, because of the metrics are not updated in the latest 5 minutes, and the usage function is invalid.",
+				PodGroups: []*schedulingv1.PodGroup{pg1},
+				Queues:    []*schedulingv1.Queue{queue1},
+				Pods:      []*v1.Pod{p1, p2},
+				Nodes:     []*v1.Node{n4},
 			},
 			nodesUsageMap: nodesUsage,
 			arguments: framework.Arguments{
@@ -266,18 +214,12 @@ func TestUsage_predicateFn(t *testing.T) {
 			},
 		},
 		{
-			name: "The node can be scheduled, because of the metric time is in the initial state, and the usage function is invalid.",
-			podGroups: []*schedulingv1.PodGroup{
-				pg1,
-			},
-			queues: []*schedulingv1.Queue{
-				queue1,
-			},
-			pods: []*v1.Pod{
-				p1, p2,
-			},
-			nodes: []*v1.Node{
-				n5,
+			TestCommonStruct: uthelper.TestCommonStruct{
+				Name:      "The node can be scheduled, because of the metric time is in the initial state, and the usage function is invalid.",
+				PodGroups: []*schedulingv1.PodGroup{pg1},
+				Queues:    []*schedulingv1.Queue{queue1},
+				Pods:      []*v1.Pod{p1, p2},
+				Nodes:     []*v1.Node{n5},
 			},
 			nodesUsageMap: nodesUsage,
 			arguments: framework.Arguments{
@@ -302,33 +244,9 @@ func TestUsage_predicateFn(t *testing.T) {
 	}
 
 	for i, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			schedulerCache := &cache.SchedulerCache{
-				Nodes:         make(map[string]*api.NodeInfo),
-				Jobs:          make(map[api.JobID]*api.JobInfo),
-				Queues:        make(map[api.QueueID]*api.QueueInfo),
-				StatusUpdater: &util.FakeStatusUpdater{},
-				VolumeBinder:  &util.FakeVolumeBinder{},
-
-				Recorder: record.NewFakeRecorder(100),
-			}
-
-			for _, node := range test.nodes {
-				schedulerCache.AddOrUpdateNode(node)
-			}
-			for _, pod := range test.pods {
-				schedulerCache.AddPod(pod)
-			}
-			for _, ss := range test.podGroups {
-				schedulerCache.AddPodGroupV1beta1(ss)
-			}
-			for _, q := range test.queues {
-				schedulerCache.AddQueueV1beta1(q)
-			}
-			updateNodeUsage(schedulerCache.Nodes, nodesUsage)
-
+		t.Run(test.Name, func(t *testing.T) {
 			trueValue := true
-			ssn := framework.OpenSession(schedulerCache, []conf.Tier{
+			tiers := []conf.Tier{
 				{
 					Plugins: []conf.PluginOption{
 						{
@@ -338,8 +256,12 @@ func TestUsage_predicateFn(t *testing.T) {
 						},
 					},
 				},
-			}, nil)
-			defer framework.CloseSession(ssn)
+			}
+			test.Plugins = plugins
+			ssn := test.RegisterSession(tiers, nil)
+			defer test.Close()
+
+			updateNodeUsage(ssn.Nodes, nodesUsage)
 
 			for _, job := range ssn.Jobs {
 				for _, task := range job.Tasks {
@@ -351,16 +273,31 @@ func TestUsage_predicateFn(t *testing.T) {
 								i, taskID, node.Name, test.expected.err, err)
 							continue
 						}
-						if test.expected.err != nil && test.expected.err.Error() != err.Error() {
-							t.Errorf("case%d: task %s on node %s has error, expect: %v, actual: %v",
-								i, taskID, node.Name, test.expected.err, err)
+						if err == nil {
 							continue
 						}
-						predicateStatus := err.(*api.FitError).Status
+
+						fitErr := err.(*api.FitError)
+						predicateStatus := fitErr.Status
+
+						errString := func(fe *api.FitError) string {
+							if len(fe.Status) > 0 {
+								return fmt.Sprintf("plugin %s predicates failed, because of %s", fe.Status[0].Plugin, strings.Join(fe.Reasons(), ", "))
+							}
+							return strings.Join(fe.Reasons(), ", ")
+						}
+
+						errStr := errString(fitErr)
+						if test.expected.err != nil && test.expected.err.Error() != errStr {
+							t.Errorf("case%d: task %s on node %s has error:\nexpect: %v\nactual: %v",
+								i, taskID, node.Name, test.expected.err, errStr)
+							continue
+						}
+
 						for index := range predicateStatus {
 							if predicateStatus[index].Code != test.expected.predicateStatus[index].Code ||
 								predicateStatus[index].Reason != test.expected.predicateStatus[index].Reason {
-								t.Errorf("case%d: task %s on node %s has error, expect: %v, actual: %v",
+								t.Errorf("case%d: task %s on node %s has error:\nexpect: %v\nactual: %v",
 									i, taskID, node.Name, test.expected.predicateStatus[index], predicateStatus[index])
 								continue
 							}
@@ -373,14 +310,7 @@ func TestUsage_predicateFn(t *testing.T) {
 }
 
 func TestUsage_nodeOrderFn(t *testing.T) {
-	var tmp *cache.SchedulerCache
-	patchUpdateQueueStatus := gomonkey.ApplyMethod(reflect.TypeOf(tmp), "UpdateQueueStatus", func(scCache *cache.SchedulerCache, queue *api.QueueInfo) error {
-		return nil
-	})
-	defer patchUpdateQueueStatus.Reset()
-
-	framework.RegisterPluginBuilder(PluginName, New)
-	defer framework.CleanupPluginBuilders()
+	plugins := map[string]framework.PluginBuilder{PluginName: New}
 
 	p1 := util.BuildPod("c1", "p1", "", v1.PodPending, api.BuildResourceList("1", "1Gi"), "pg1", make(map[string]string), make(map[string]string))
 
@@ -402,47 +332,31 @@ func TestUsage_nodeOrderFn(t *testing.T) {
 	// The node score is 0.
 	nodesUsage[n5.Name] = buildNodeUsage(map[string]float64{source.NODE_METRICS_PERIOD: 0}, map[string]float64{source.NODE_METRICS_PERIOD: 0}, time.Time{})
 
-	pg1 := &schedulingv1.PodGroup{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pg1",
-			Namespace: "c1",
-		},
-		Spec: schedulingv1.PodGroupSpec{
-			Queue: "q1",
-		},
-	}
-	queue1 := &schedulingv1.Queue{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "q1",
-		},
-		Spec: schedulingv1.QueueSpec{
-			Weight: 1,
-		},
-	}
+	pg1 := util.BuildPodGroup("pg1", "c1", "q1", 0, nil, "")
+
+	queue1 := util.BuildQueue("q1", 1, nil)
 
 	tests := []struct {
-		name          string
-		podGroups     []*schedulingv1.PodGroup
-		queues        []*schedulingv1.Queue
-		pods          []*v1.Pod
-		nodes         []*v1.Node
+		uthelper.TestCommonStruct
 		nodesUsageMap map[string]*api.NodeUsage
 		arguments     framework.Arguments
 		expected      map[string]map[string]float64
 	}{
 		{
-			name: "Node scoring in the default weight configuration scenario.",
-			podGroups: []*schedulingv1.PodGroup{
-				pg1,
-			},
-			queues: []*schedulingv1.Queue{
-				queue1,
-			},
-			pods: []*v1.Pod{
-				p1,
-			},
-			nodes: []*v1.Node{
-				n1, n2, n3, n4, n5,
+			TestCommonStruct: uthelper.TestCommonStruct{
+				Name: "Node scoring in the default weight configuration scenario.",
+				PodGroups: []*schedulingv1.PodGroup{
+					pg1,
+				},
+				Queues: []*schedulingv1.Queue{
+					queue1,
+				},
+				Pods: []*v1.Pod{
+					p1,
+				},
+				Nodes: []*v1.Node{
+					n1, n2, n3, n4, n5,
+				},
 			},
 			nodesUsageMap: nodesUsage,
 			arguments: framework.Arguments{
@@ -465,18 +379,20 @@ func TestUsage_nodeOrderFn(t *testing.T) {
 			},
 		},
 		{
-			name: "Node scoring gives priority to memory resources",
-			podGroups: []*schedulingv1.PodGroup{
-				pg1,
-			},
-			queues: []*schedulingv1.Queue{
-				queue1,
-			},
-			pods: []*v1.Pod{
-				p1,
-			},
-			nodes: []*v1.Node{
-				n1, n2, n3, n4, n5,
+			TestCommonStruct: uthelper.TestCommonStruct{
+				Name: "Node scoring gives priority to memory resources",
+				PodGroups: []*schedulingv1.PodGroup{
+					pg1,
+				},
+				Queues: []*schedulingv1.Queue{
+					queue1,
+				},
+				Pods: []*v1.Pod{
+					p1,
+				},
+				Nodes: []*v1.Node{
+					n1, n2, n3, n4, n5,
+				},
 			},
 			nodesUsageMap: nodesUsage,
 			arguments: framework.Arguments{
@@ -501,33 +417,9 @@ func TestUsage_nodeOrderFn(t *testing.T) {
 	}
 
 	for i, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			schedulerCache := &cache.SchedulerCache{
-				Nodes:         make(map[string]*api.NodeInfo),
-				Jobs:          make(map[api.JobID]*api.JobInfo),
-				Queues:        make(map[api.QueueID]*api.QueueInfo),
-				StatusUpdater: &util.FakeStatusUpdater{},
-				VolumeBinder:  &util.FakeVolumeBinder{},
-
-				Recorder: record.NewFakeRecorder(100),
-			}
-
-			for _, node := range test.nodes {
-				schedulerCache.AddOrUpdateNode(node)
-			}
-			for _, pod := range test.pods {
-				schedulerCache.AddPod(pod)
-			}
-			for _, ss := range test.podGroups {
-				schedulerCache.AddPodGroupV1beta1(ss)
-			}
-			for _, q := range test.queues {
-				schedulerCache.AddQueueV1beta1(q)
-			}
-			updateNodeUsage(schedulerCache.Nodes, nodesUsage)
-
+		t.Run(test.Name, func(t *testing.T) {
 			trueValue := true
-			ssn := framework.OpenSession(schedulerCache, []conf.Tier{
+			tiers := []conf.Tier{
 				{
 					Plugins: []conf.PluginOption{
 						{
@@ -537,8 +429,12 @@ func TestUsage_nodeOrderFn(t *testing.T) {
 						},
 					},
 				},
-			}, nil)
-			defer framework.CloseSession(ssn)
+			}
+			test.Plugins = plugins
+			ssn := test.RegisterSession(tiers, nil)
+			defer test.Close()
+
+			updateNodeUsage(ssn.Nodes, nodesUsage)
 
 			for _, job := range ssn.Jobs {
 				for _, task := range job.Tasks {
