398501ec784509af0f78e02e9d28df27ece9b45e
limengxuan <391013634@qq.com>
2024-12-17 17:48:56 +0800

    update design docs for dynamic-volcano
    Signed-off-by: limengxuan <391013634@qq.com>

--- pkg/scheduler/api/devices/nvidia/vgpu/type.go
+++ pkg/scheduler/api/devices/nvidia/vgpu/type.go
@@ -16,9 +16,6 @@ limitations under the License.
 
 package vgpu
 
-var VGPUEnable bool
-var NodeLockEnable bool
-
 const (
 	GPUInUse                         = "nvidia.com/use-gputype"
 	GPUNoUse                         = "nvidia.com/nouse-gputype"
@@ -64,6 +61,14 @@ const (
 	DefaultMemPercentage = 101
 	binpackMultiplier    = 100
 	spreadMultiplier     = 100
+
+	vGPUControllerHAMICore = "hami-core"
+	vGPUControllerMIG      = "mig"
+)
+
+var (
+	VGPUEnable     bool
+	NodeLockEnable bool
 )
 
 type ContainerDeviceRequest struct {
