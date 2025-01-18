538ab6eea5dd556b1c2f303be74527e229bfaac8
Monokaix <changxuzheng@huawei.com>
2025-01-18 18:31:02 +0800

    [bugfix]fix leader-elect-resource-namespace flag not take effect
    Signed-off-by: Monokaix <changxuzheng@huawei.com>

--- cmd/controller-manager/app/options/options_test.go
+++ cmd/controller-manager/app/options/options_test.go
@@ -99,10 +99,9 @@ func TestAddFlags(t *testing.T) {
 			ResourceNamespace: defaultLockObjectNamespace,
 			ResourceName:      "vc-controller-manager",
 		},
-		LockObjectNamespace: defaultLockObjectNamespace,
-		WorkerThreadsForPG:  5,
-		WorkerThreadsForGC:  1,
-		Controllers:         []string{"*"},
+		WorkerThreadsForPG: 5,
+		WorkerThreadsForGC: 1,
+		Controllers:        []string{"*"},
 	}
 	expectedFeatureGates := map[featuregate.Feature]bool{features.ResourceTopology: false}
 
