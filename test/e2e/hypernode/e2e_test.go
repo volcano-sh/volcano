292242a0e3ab4aba6604fb62b922f089e0c96f04
xuwentao <cutenear1993@yahoo.com>
2025-01-23 01:25:38 +0800

    test: add e2e test cases for hypernode
    Signed-off-by: xuwentao <cutenear1993@yahoo.com>

---
+++ test/e2e/hypernode/e2e_test.go
@@ -0,0 +1,13 @@
+package hypernode
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestE2E(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Volcano Network Topology Test Suite")
+}
