f49301a0e6d78ff8caf6dfb66d2ef895793f8160
SataQiu <shidaqiu2018@gmail.com>
2025-03-27 14:12:14 +0800

    fix bug for vgpu type check
    Signed-off-by: SataQiu <shidaqiu2018@gmail.com>

--- pkg/scheduler/api/devices/nvidia/vgpu/utils.go
+++ pkg/scheduler/api/devices/nvidia/vgpu/utils.go
@@ -280,7 +280,7 @@ func checkGPUtype(annos map[string]string, cardtype string) bool {
 	if ok {
 		if !strings.Contains(nouse, ",") {
 			if strings.Contains(strings.ToUpper(cardtype), strings.ToUpper(nouse)) {
-				return true
+				return false
 			}
 		} else {
 			for _, val := range strings.Split(nouse, ",") {
