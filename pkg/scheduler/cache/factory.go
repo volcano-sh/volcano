75a74f8f4578cc9feb36d92d31abc7c8f1042607
JesseStutler <chenzicong4@huawei.com>
2025-03-29 17:27:17 +0800

    Add unified kube scheduler plugin and refactor volume binding
    Signed-off-by: JesseStutler <chenzicong4@huawei.com>

--- pkg/scheduler/cache/factory.go
+++ pkg/scheduler/cache/factory.go
@@ -1,5 +1,7 @@
 package cache
 
+import "sync"
+
 // bindMethodMap Binder management
 var bindMethodMap Binder
 
@@ -11,4 +13,36 @@ func RegisterBindMethod(binder Binder) {
 // GetBindMethod get the registered Binder
 func GetBindMethod() Binder {
 	return bindMethodMap
+}
+
+type bindHandler struct {
+	preBinder PreBinder
+}
+
+// BindHandlerRegistry is used to hold the registered bindHandlers, which include calls to PreBind,
+// error handling for bind failures, etc.
+type BindHandlerRegistry struct {
+	mu       sync.RWMutex
+	handlers map[string]*bindHandler
+}
+
+func NewBindHandlerRegistry() *BindHandlerRegistry {
+	return &BindHandlerRegistry{
+		handlers: make(map[string]*bindHandler),
+	}
+}
+
+// Register registers or updates a bind handler for the given plugin name.
+// It always overwrites the existing handler to support plugin configuration updates
+// during runtime, as plugins may be reconfigured without restarting the scheduler.
+func (r *BindHandlerRegistry) Register(name string, handler interface{}) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	h := &bindHandler{}
+	if pb, ok := handler.(PreBinder); ok {
+		h.preBinder = pb
+	}
+
+	r.handlers[name] = h
 }
--- pkg/scheduler/plugins/factory.go
+++ pkg/scheduler/plugins/factory.go
@@ -26,6 +26,7 @@ import (
 	"volcano.sh/volcano/pkg/scheduler/plugins/drf"
 	"volcano.sh/volcano/pkg/scheduler/plugins/extender"
 	"volcano.sh/volcano/pkg/scheduler/plugins/gang"
+	"volcano.sh/volcano/pkg/scheduler/plugins/kubescheduler"
 	"volcano.sh/volcano/pkg/scheduler/plugins/nodegroup"
 	"volcano.sh/volcano/pkg/scheduler/plugins/nodeorder"
 	"volcano.sh/volcano/pkg/scheduler/plugins/numaaware"
@@ -62,6 +63,7 @@ func init() {
 	framework.RegisterPluginBuilder(usage.PluginName, usage.New)
 	framework.RegisterPluginBuilder(pdb.PluginName, pdb.New)
 	framework.RegisterPluginBuilder(nodegroup.PluginName, nodegroup.New)
+	framework.RegisterPluginBuilder(kubescheduler.PluginName, kubescheduler.New)
 
 	// Plugins for Queues
 	framework.RegisterPluginBuilder(proportion.PluginName, proportion.New)
