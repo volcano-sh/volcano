27d5a66c09923dab03827fb23e745fc6e53526cd
wangdongyang1 <wangdongyang1@sensetime.com>
2025-05-23 10:12:51 +0800

    task rank and node ip aware
    Signed-off-by: wangdongyang1 <wangdongyang1@sensetime.com>

--- pkg/scheduler/api/job_info.go
+++ pkg/scheduler/api/job_info.go
@@ -241,6 +241,21 @@ func (ti *TaskInfo) ClearLastTxContext() {
 	ti.LastTransaction = nil
 }
 
+func (ti *TaskInfo) GetRank() string {
+	if ti == nil || ti.Pod == nil {
+		return ""
+	}
+	for _, c := range ti.Pod.Spec.Containers {
+		for _, env := range c.Env {
+			if env.Name == "RANK" {
+				klog.V(4).Infof(" pod %v/%v get rank %v", ti.Namespace, ti.Name, env.Value)
+				return env.Value
+			}
+		}
+	}
+	return ""
+}
+
 // Return if the pod of a task is scheduling gated by checking if length of sch gates is zero
 // When the Pod is not yet created or sch gates field not set, return false
 func calSchedulingGated(pod *v1.Pod) bool {
