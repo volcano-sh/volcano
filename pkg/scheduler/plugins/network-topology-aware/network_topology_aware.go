56ff2fe6cb6198ec2fd6d4a5f103a1f70ccc0b58
wangdongyang1 <wangdongyang1@sensetime.com>
2025-05-27 09:13:21 +0800

    fix import
    Signed-off-by: wangdongyang1 <wangdongyang1@sensetime.com>

--- pkg/scheduler/plugins/network-topology-aware/network_topology_aware.go
+++ pkg/scheduler/plugins/network-topology-aware/network_topology_aware.go
@@ -24,6 +24,7 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
+
 	"volcano.sh/volcano/pkg/scheduler/api"
 	"volcano.sh/volcano/pkg/scheduler/framework"
 	"volcano.sh/volcano/pkg/scheduler/util"
