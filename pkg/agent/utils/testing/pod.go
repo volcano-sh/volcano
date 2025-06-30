764547fe435df46223f7f66aafd9884b02656538
mahdikhashan <mahdikhashan1@gmail.com>
2025-06-30 13:29:36 +0200

    fix goimports

--- pkg/agent/utils/testing/pod.go
+++ pkg/agent/utils/testing/pod.go
@@ -19,7 +19,7 @@ package testing
 import (
 	"context"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/record"
