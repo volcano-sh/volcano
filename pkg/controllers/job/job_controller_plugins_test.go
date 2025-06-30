d7990d0875c11aaeaaf99f58217dec998a5a1fc9
mahdikhashan <mahdikhashan1@gmail.com>
2025-06-30 13:56:32 +0200

    remove newline
    Signed-off-by: mahdikhashan <mahdikhashan1@gmail.com>

--- pkg/controllers/job/job_controller_plugins_test.go
+++ pkg/controllers/job/job_controller_plugins_test.go
@@ -286,7 +286,6 @@ func TestPluginOnJobDelete(t *testing.T) {
 			}
 
 			for _, plugin := range testcase.Plugins {
-
 				if plugin == "svc" {
 					_, err := fakeController.kubeClient.CoreV1().ConfigMaps(namespace).Get(context.TODO(), fmt.Sprint(testcase.Job.Name, "-svc"), metav1.GetOptions{})
 					if err == nil {
