011c6dc6da1bea92eabfebf8faf56ffe5339fbdd
常在河边走 <shoted@outlook.com>
2025-08-26 22:43:37 +0800

    fix: vgpu memory allocate wrong when use volcano.sh/vgpu-memory-percentage

--- pkg/scheduler/api/devices/nvidia/vgpu/utils.go
+++ pkg/scheduler/api/devices/nvidia/vgpu/utils.go
@@ -387,7 +387,8 @@ func checkNodeGPUSharingPredicateAndScore(pod *v1.Pod, gssnap *GPUDevices, repli
 				continue
 			}
 			if val.MemPercentagereq != 101 && val.Memreq == 0 {
-				val.Memreq = gs.Device[i].Memory * uint(val.MemPercentagereq/100)
+				percentage := float64(val.MemPercentagereq) / 100
+				val.Memreq = uint(float64(gs.Device[i].Memory) * percentage)
 			}
 			if int(gs.Device[i].Memory)-int(gs.Device[i].UsedMem) < int(val.Memreq) {
 				continue
