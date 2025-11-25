2a16a2619c9400552a2ed9f0f4ae44b8727b0006
handan-yxh <yxh953167434@163.com>
2025-11-25 21:55:11 +0800

    FE: Add a new agent-scheduler
    Signed-off-by: handan-yxh <yxh953167434@163.com>

---
+++ cmd/agent-scheduler/app/server.go
@@ -0,0 +1,197 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/pprof"
+	"os"
+
+	"volcano.sh/apis/pkg/apis/helpers"
+	"volcano.sh/volcano/cmd/agent-scheduler/app/options"
+	"volcano.sh/volcano/pkg/agentscheduler"
+	"volcano.sh/volcano/pkg/kube"
+	"volcano.sh/volcano/pkg/signals"
+	commonutil "volcano.sh/volcano/pkg/util"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/klog/v2"
+
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
+
+	// Register gcp auth
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	"k8s.io/client-go/tools/record"
+	basecompatibility "k8s.io/component-base/compatibility"
+
+	// Register rest client metrics
+	_ "k8s.io/component-base/metrics/prometheus/restclient"
+)
+
+// Run the volcano scheduler.
+func Run(opt *options.ServerOption) error {
+	config, err := kube.BuildConfig(opt.KubeClientOptions)
+	if err != nil {
+		return err
+	}
+
+	// Align default feature-gates with the connected cluster's version.
+	if err := setupComponentGlobals(config); err != nil {
+		klog.Errorf("failed to set component globals: %v", err)
+		return err
+	}
+
+	sched, err := agentscheduler.NewAgentScheduler(config, opt)
+	if err != nil {
+		panic(err)
+	}
+
+	if opt.EnableHealthz {
+		if err := helpers.StartHealthz(opt.HealthzBindAddress, "volcano-agent-scheduler", opt.CaCertData, opt.CertData, opt.KeyData); err != nil {
+			return err
+		}
+	}
+
+	ctx := signals.SetupSignalContext()
+	run := func(ctx context.Context) {
+		sched.Run(ctx.Done())
+		<-ctx.Done()
+	}
+
+	if !opt.LeaderElection.LeaderElect {
+		run(ctx)
+		return fmt.Errorf("finished without leader elect")
+	}
+
+	leaderElectionClient, err := clientset.NewForConfig(restclient.AddUserAgent(config, "leader-election"))
+	if err != nil {
+		return err
+	}
+
+	// Prepare event clients.
+	broadcaster := record.NewBroadcaster()
+	broadcaster.StartRecordingToSink(&corev1.EventSinkImpl{Interface: leaderElectionClient.CoreV1().Events(opt.LeaderElection.ResourceNamespace)})
+	eventRecorder := broadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: commonutil.GenerateComponentName(opt.SchedulerNames)})
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		return fmt.Errorf("unable to get hostname: %v", err)
+	}
+	// add a uniquifier so that two processes on the same host don't accidentally both become active
+	id := hostname + "_" + string(uuid.NewUUID())
+	// set ResourceNamespace value to LockObjectNamespace when it's not empty,compatible with old flag
+	//lint:ignore SA1019 LockObjectNamespace is deprecated and will be removed in a future release
+	if len(opt.LockObjectNamespace) > 0 {
+		//lint:ignore SA1019 LockObjectNamespace is deprecated and will be removed in a future release
+		opt.LeaderElection.ResourceNamespace = opt.LockObjectNamespace
+	}
+	rl, err := resourcelock.New(resourcelock.LeasesResourceLock,
+		opt.LeaderElection.ResourceNamespace,
+		opt.LeaderElection.ResourceName,
+		leaderElectionClient.CoreV1(),
+		leaderElectionClient.CoordinationV1(),
+		resourcelock.ResourceLockConfig{
+			Identity:      id,
+			EventRecorder: eventRecorder,
+		})
+	if err != nil {
+		return fmt.Errorf("couldn't create resource lock: %v", err)
+	}
+
+	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
+		Lock:          rl,
+		LeaseDuration: opt.LeaderElection.LeaseDuration.Duration,
+		RenewDeadline: opt.LeaderElection.RenewDeadline.Duration,
+		RetryPeriod:   opt.LeaderElection.RetryPeriod.Duration,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: run,
+			OnStoppedLeading: func() {
+				klog.Fatalf("leaderelection lost")
+			},
+		},
+	})
+	return fmt.Errorf("lost lease")
+}
+
+func startMetricsServer(opt *options.ServerOption) {
+	mux := http.NewServeMux()
+
+	if opt.EnableMetrics {
+		mux.Handle("/metrics", commonutil.PromHandler())
+	}
+
+	if opt.EnablePprof {
+		mux.HandleFunc("/debug/pprof/", pprof.Index)
+		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	}
+
+	server := &http.Server{
+		Addr:              opt.ListenAddress,
+		Handler:           mux,
+		ReadHeaderTimeout: helpers.DefaultReadHeaderTimeout,
+		ReadTimeout:       helpers.DefaultReadTimeout,
+		WriteTimeout:      helpers.DefaultWriteTimeout,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		klog.Errorf("start metrics/pprof http server failed: %v", err)
+	}
+}
+
+// setupComponentGlobals discovers the API server version and sets
+// Volcano's effective version + feature gate defaults to match the cluster.
+// This makes defaults (like DRA) correct on older clusters.
+func setupComponentGlobals(config *restclient.Config) error {
+	client, err := clientset.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	serverVersion, err := client.Discovery().ServerVersion()
+	if err != nil {
+		return err
+	}
+
+	kubeVersion := fmt.Sprintf("%s.%s", serverVersion.Major, serverVersion.Minor)
+	kubeEffectiveVersion := basecompatibility.NewEffectiveVersionFromString(kubeVersion, "", "")
+
+	componentGlobalsRegistry := basecompatibility.NewComponentGlobalsRegistry()
+	if componentGlobalsRegistry.EffectiveVersionFor(basecompatibility.DefaultKubeComponent) == nil {
+		err = componentGlobalsRegistry.Register(
+			basecompatibility.DefaultKubeComponent,
+			kubeEffectiveVersion,
+			utilfeature.DefaultMutableFeatureGate,
+		)
+		if err != nil {
+			return fmt.Errorf("failed to register component globals: %w", err)
+		}
+	}
+
+	return componentGlobalsRegistry.Set()
+}
---
+++ pkg/agentscheduler/cache/cache_mock.go
@@ -0,0 +1,168 @@
+/*
+Copyright 2023 The Volcano Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cache
+
+import (
+	"context"
+	"math"
+	"os"
+	"strconv"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+	resourceslicetracker "k8s.io/dynamic-resource-allocation/resourceslice/tracker"
+	"k8s.io/klog/v2"
+	kubefeatures "k8s.io/kubernetes/pkg/features"
+	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/dynamicresources"
+	"k8s.io/kubernetes/pkg/scheduler/util/assumecache"
+
+	fakevcClient "volcano.sh/apis/pkg/client/clientset/versioned/fake"
+	"volcano.sh/volcano/cmd/agent-scheduler/app/options"
+	schedulingapi "volcano.sh/volcano/pkg/scheduler/api"
+)
+
+// NewCustomMockSchedulerCache returns a mock scheduler cache with custom interface
+func NewCustomMockSchedulerCache(schedulerName string,
+	binder Binder,
+	evictor Evictor,
+	statusUpdater StatusUpdater,
+	PodGroupBinder BatchBinder,
+	recorder record.EventRecorder,
+) *SchedulerCache {
+	msc := newMockSchedulerCache(schedulerName)
+	// add all events handlers
+	msc.addEventHandler()
+	msc.Recorder = recorder
+	msc.Binder = binder
+	msc.Evictor = evictor
+	msc.StatusUpdater = statusUpdater
+	checkAndSetDefaultInterface(msc)
+	return msc
+}
+
+// NewDefaultMockSchedulerCache returns a mock scheduler cache with interface mocked with default fake clients
+// Notes that default events recorder's buffer only has a length 100;
+// when use it do performance test, should use a &FakeRecorder{} without length limit to avoid block
+func NewDefaultMockSchedulerCache(schedulerName string) *SchedulerCache {
+	msc := newMockSchedulerCache(schedulerName)
+	// add all events handlers
+	msc.addEventHandler()
+	checkAndSetDefaultInterface(msc)
+	return msc
+}
+
+func checkAndSetDefaultInterface(sc *SchedulerCache) {
+	if sc.Recorder == nil {
+		sc.Recorder = record.NewFakeRecorder(100) // to avoid blocking, we can pass in &FakeRecorder{} to NewCustomMockSchedulerCache
+	}
+	if sc.Binder == nil {
+		sc.Binder = &DefaultBinder{
+			kubeclient: sc.kubeClient,
+			recorder:   sc.Recorder,
+		}
+	}
+	if sc.Evictor == nil {
+		sc.Evictor = &defaultEvictor{
+			kubeclient: sc.kubeClient,
+			recorder:   sc.Recorder,
+		}
+	}
+	if sc.StatusUpdater == nil {
+		sc.StatusUpdater = &defaultStatusUpdater{
+			kubeclient: sc.kubeClient,
+			vcclient:   sc.vcClient,
+		}
+	}
+}
+
+func getNodeWorkers() uint32 {
+	if options.ServerOpts != nil && options.ServerOpts.NodeWorkerThreads > 0 {
+		return options.ServerOpts.NodeWorkerThreads
+	}
+	threads, err := strconv.Atoi(os.Getenv("NODE_WORKER_THREADS"))
+	if err == nil && threads > 0 && threads <= math.MaxUint32 {
+		return uint32(threads)
+	}
+	return 2 //default 2
+}
+
+// newMockSchedulerCache init the mock scheduler cache structure
+func newMockSchedulerCache(schedulerName string) *SchedulerCache {
+	msc := &SchedulerCache{
+		Nodes:               make(map[string]*schedulingapi.NodeInfo),
+		errTasks:            workqueue.NewTypedRateLimitingQueue[string](workqueue.DefaultTypedControllerRateLimiter[string]()),
+		nodeQueue:           workqueue.NewTypedRateLimitingQueue[string](workqueue.DefaultTypedControllerRateLimiter[string]()),
+		DeletedJobs:         workqueue.NewTypedRateLimitingQueue[*schedulingapi.JobInfo](workqueue.DefaultTypedControllerRateLimiter[*schedulingapi.JobInfo]()),
+		kubeClient:          fake.NewSimpleClientset(),
+		vcClient:            fakevcClient.NewSimpleClientset(),
+		restConfig:          nil,
+		schedulerNames:      []string{schedulerName},
+		nodeSelectorLabels:  make(map[string]sets.Empty),
+		NamespaceCollection: make(map[string]*schedulingapi.NamespaceCollection),
+		imageStates:         make(map[string]*imageState),
+
+		NodeList:       []string{},
+		binderRegistry: NewBinderRegistry(),
+		resyncPeriod:   0,
+	}
+	if options.ServerOpts != nil && len(options.ServerOpts.NodeSelector) > 0 {
+		msc.updateNodeSelectors(options.ServerOpts.NodeSelector)
+	}
+	msc.setBatchBindParallel()
+	msc.nodeWorkers = getNodeWorkers()
+	msc.initMockInformers()
+
+	return msc
+}
+
+// initMockInformers initializes the informer factory and DRA manager for mock cache
+func (sc *SchedulerCache) initMockInformers() {
+	// Initialize informer factory with fake client
+	informerFactory := informers.NewSharedInformerFactory(sc.kubeClient, sc.resyncPeriod)
+	sc.informerFactory = informerFactory
+
+	// Create node informer
+	sc.nodeInformer = informerFactory.Core().V1().Nodes()
+
+	// Initialize DRA manager if feature is enabled
+	if utilfeature.DefaultFeatureGate.Enabled(kubefeatures.DynamicResourceAllocation) {
+		ctx := context.TODO()
+		logger := klog.FromContext(ctx)
+		resourceClaimInformer := informerFactory.Resource().V1().ResourceClaims().Informer()
+		resourceClaimCache := assumecache.NewAssumeCache(logger, resourceClaimInformer, "ResourceClaim", "", nil)
+		resourceSliceTrackerOpts := resourceslicetracker.Options{
+			EnableDeviceTaints: utilfeature.DefaultFeatureGate.Enabled(kubefeatures.DRADeviceTaints),
+			SliceInformer:      informerFactory.Resource().V1().ResourceSlices(),
+			KubeClient:         sc.kubeClient,
+		}
+		// If device taints are disabled, the additional informers are not needed and
+		// the tracker turns into a simple wrapper around the slice informer.
+		if resourceSliceTrackerOpts.EnableDeviceTaints {
+			resourceSliceTrackerOpts.TaintInformer = informerFactory.Resource().V1alpha3().DeviceTaintRules()
+			resourceSliceTrackerOpts.ClassInformer = informerFactory.Resource().V1().DeviceClasses()
+		}
+		resourceSliceTracker, err := resourceslicetracker.StartTracker(ctx, resourceSliceTrackerOpts)
+		if err != nil {
+			klog.V(3).Infof("couldn't start resource slice tracker: %v", err)
+		}
+		sc.sharedDRAManager = dynamicresources.NewDRAManager(ctx, resourceClaimCache, resourceSliceTracker, informerFactory)
+	}
+}
