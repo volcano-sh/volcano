580b6469deced3b912d3982a56b4eea28a02971e
handan-yxh <yxh953167434@163.com>
2025-12-25 12:33:05 +0800

    add kubernetes author header
    Signed-off-by: handan-yxh <yxh953167434@163.com>

--- pkg/agentscheduler/cache/cache.go
+++ pkg/agentscheduler/cache/cache.go
@@ -1,4 +1,5 @@
 /*
+ Copyright 2019 The Kubernetes Authors.
  Copyright 2025 The Volcano Authors.
 
  Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +13,10 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
+
+This file includes code adapted from the Kubernetes scheduler cache
+implementation (https://github.com/kubernetes/kubernetes/blob/master/pkg/scheduler/framework/types.go),
+with modifications made by the Volcano Authors.
 */
 
 package cache
