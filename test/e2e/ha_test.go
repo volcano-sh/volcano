fb8f679e85c7cc8c058b8a21a5368ac12162c0bf
Joshua Pella <joshuapellaa@gmail.com>
2026-03-31 15:59:11 +0100

    test: fix context initialization, imports, and ginkgo assertions based on review
    Signed-off-by: Joshua Pella <joshuapellaa@gmail.com>

--- test/e2e/ha_test.go
+++ test/e2e/ha_test.go
@@ -7,19 +7,33 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	e2eutil "volcano.sh/volcano/test/e2e/util"
 )
 
 var _ = Describe("High Availability Deployment Tests", func() {
+	var ctx *e2eutil.TestContext
+
+	BeforeEach(func() {
+		ctx = e2eutil.InitTestContext(e2eutil.Options{})
+	})
+
+	AfterEach(func() {
+		e2eutil.CleanupTestContext(ctx)
+	})
+
 	It("Should successfully spin up and report 2 ready replicas for core components", func() {
 		components := []string{"volcano-scheduler", "volcano-controllers", "volcano-admission"}
 		namespace := "volcano-system"
 
 		for _, component := range components {
-			Eventually(func() int32 {
-				deployment, err := ctx.KubeClient().AppsV1().Deployments(namespace).Get(context.TODO(), component, metav1.GetOptions{})
-				Expect(err).NotTo(HaveOccurred(), "Failed to get deployment for %s", component)
+			Eventually(func() (int32, error) {
+				deployment, err := ctx.Kubeclient.AppsV1().Deployments(namespace).Get(context.TODO(), component, metav1.GetOptions{})
+				if err != nil {
+					return 0, err
+				}
 				
-				return deployment.Status.ReadyReplicas
+				return deployment.Status.ReadyReplicas, nil
 			}, 3*time.Minute, 5*time.Second).Should(Equal(int32(2)), "Component %s did not reach 2 ready replicas", component)
 		}
 	})
