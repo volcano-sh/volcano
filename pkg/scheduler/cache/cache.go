38fab65b8bfd01481cfc8a3ab2a8506342a32ff0
qi-min <qim_34@163.com>
2026-04-13 17:04:53 +0800

    record event handlers
    Signed-off-by: qi-min <qim_34@163.com>

--- pkg/scheduler/cache/cache.go
+++ pkg/scheduler/cache/cache.go
@@ -845,6 +845,7 @@ func (sc *SchedulerCache) addEventHandler() {
 		}
 		sc.sharedDRAManager = dynamicresources.NewDRAManager(ctx, resourceClaimCache, resourceSliceTracker, informerFactory)
 	}
+	sc.registeredHandlers = handlers
 }
 
 // Run  starts the schedulerCache
