a9d1c1b87a595497807731150812e0cb477a6c9e
devzizu <jazevedo960@gmail.com>
2026-04-16 15:58:23 +0100

    refactor: move schGateManager to new schedulinggate.go
    Signed-off-by: devzizu <jazevedo960@gmail.com>

--- pkg/scheduler/actions/allocate/allocate.go
+++ pkg/scheduler/actions/allocate/allocate.go
@@ -25,7 +25,6 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/sets"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
 
 	"volcano.sh/apis/pkg/apis/scheduling"
@@ -33,7 +32,6 @@ import (
 	"volcano.sh/volcano/cmd/scheduler/app/options"
 	"volcano.sh/volcano/pkg/features"
 	"volcano.sh/volcano/pkg/scheduler/api"
-	"volcano.sh/volcano/pkg/scheduler/cache"
 	"volcano.sh/volcano/pkg/scheduler/conf"
 	"volcano.sh/volcano/pkg/scheduler/framework"
 	"volcano.sh/volcano/pkg/scheduler/metrics"
@@ -102,93 +100,6 @@ func (w *SubJobWorksheet) Empty() bool {
 func (w *SubJobWorksheet) Clone() *SubJobWorksheet {
 	return &SubJobWorksheet{
 		tasks: w.tasks.Clone(),
-	}
-}
-
-// schGateManager handles asynchronous removal of Volcano scheduling gates from pods.
-// When the SchedulingGatesQueueAdmission feature is enabled, pods opt in via the
-// scheduling.volcano.sh/queue-allocation-gate annotation. The webhook injects a scheduling
-// gate at creation time, and this manager removes it asynchronously after the queue capacity
-// check passes, allowing cluster autoscalers to see the Unschedulable condition only when
-// it reflects genuine cluster resource shortage rather than queue limits.
-type schGateManager struct {
-	kubeClient   kubernetes.Interface // Cached client for worker goroutines
-	opCh         chan schGateRemovalOperation
-	workersWg    sync.WaitGroup
-	stopCh       chan struct{}
-	workerNum    int
-	shutdownOnce sync.Once
-}
-
-// schGateRemovalOperation is a request to remove the scheduling gate from a pod.
-type schGateRemovalOperation struct {
-	namespace string
-	name      string
-}
-
-func newSchGateManager(workerNum int) *schGateManager {
-	return &schGateManager{
-		stopCh:    make(chan struct{}),
-		workerNum: workerNum,
-	}
-}
-
-func (m *schGateManager) start() {
-	channelSize := m.workerNum * gateRemovalBufferPerWorker
-	m.opCh = make(chan schGateRemovalOperation, channelSize)
-
-	for i := 0; i < m.workerNum; i++ {
-		m.workersWg.Add(1)
-		go m.worker()
-	}
-	klog.V(3).Infof("Started %d async workers for gate removal", m.workerNum)
-}
-
-func (m *schGateManager) stop() {
-	m.shutdownOnce.Do(func() {
-		close(m.stopCh)
-		m.workersWg.Wait()
-		if m.opCh != nil {
-			close(m.opCh)
-		}
-		klog.V(3).Infof("Async gate removal workers shut down")
-	})
-}
-
-func (m *schGateManager) worker() {
-	defer m.workersWg.Done()
-	for {
-		select {
-		case <-m.stopCh:
-			klog.V(4).Infof("Scheduling gate operation worker shutting down")
-			return
-		case op := <-m.opCh:
-			if err := cache.RemoveVolcanoSchGate(m.kubeClient, op.namespace, op.name); err != nil {
-				klog.Errorf("Failed to remove gate from %s/%s: %v", op.namespace, op.name, err)
-			} else {
-				klog.V(3).Infof("Removed Volcano scheduling gate from pod %s/%s", op.namespace, op.name)
-			}
-		}
-	}
-}
-
-// enqueue queues an async gate removal for the given task.
-// Returns true if the operation was enqueued, false if the channel is full.
-func (m *schGateManager) enqueue(task *api.TaskInfo) bool {
-	if !api.HasOnlyVolcanoSchedulingGate(task.Pod) {
-		return false
-	}
-	op := schGateRemovalOperation{
-		namespace: task.Namespace,
-		name:      task.Name,
-	}
-	select {
-	case m.opCh <- op:
-		klog.V(3).Infof("Queued gate removal for %s/%s", task.Namespace, task.Name)
-		return true
-	default:
-		klog.Warningf("Gate operation queue full, skipping gate removal for %s/%s", task.Namespace, task.Name)
-		return false
 	}
 }
 
