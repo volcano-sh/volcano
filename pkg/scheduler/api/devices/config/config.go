66deefd0d8c16aa01f76070c931ea6d5ca6e5873
Sahil Verma <sahilvermatokyo2@gmail.com>
2026-05-28 17:29:27 +0530

    Fix confusing namespace hardcode and include error context in InitDevicesConfig log
    Signed-off-by: Sahil Verma <sahilvermatokyo2@gmail.com>

--- pkg/scheduler/api/devices/config/config.go
+++ pkg/scheduler/api/devices/config/config.go
@@ -157,8 +157,7 @@ func InitDevicesConfig(cmName, cmNamespace string) {
 		var err error
 		configs, err = loadConfigFromCM(devices.GetClient(), cmName, cmNamespace)
 		if err != nil {
-			klog.V(3).InfoS("Volcano device config not found in namespace kube-system, using default config",
-				"name", cmName)
+			klog.V(3).InfoS("Volcano device config not found, using default config", "name", cmName, "namespace", cmNamespace, "err", err)
 			configs = GetDefaultDevicesConfig()
 		}
 		klog.V(3).InfoS("Initializing volcano device config", "device-configs", configs)
