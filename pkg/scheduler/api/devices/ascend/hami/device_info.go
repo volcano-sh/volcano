8ffa0b0e48da05c6b18ee718305b1b30c1ae90be
james <open4pd@4paradigm.com>
2026-06-02 10:16:02 +0800

    fix addResource
    Signed-off-by: james <open4pd@4paradigm.com>

--- pkg/scheduler/api/devices/ascend/hami/device_info.go
+++ pkg/scheduler/api/devices/ascend/hami/device_info.go
@@ -59,6 +59,7 @@ type AscendDevice struct {
 	DeviceInfo       *devices.DeviceInfo
 	DeviceUsage      *devices.DeviceUsage
 	Score            float64
+	PodMap           map[string]*devices.DeviceUsage
 }
 
 type AscendDevices struct {
@@ -123,6 +124,7 @@ func NewAscendDevices(name string, node *v1.Node) map[string]*AscendDevices {
 					Usedmem:   0,
 					Usedcores: 0,
 				},
+				PodMap: make(map[string]*devices.DeviceUsage),
 			}
 			asDevices.Devices[nd.ID] = cur_dev
 			klog.V(5).Infof("add device. ID %s dev_info %+v", cur_dev.DeviceInfo.ID, cur_dev.DeviceInfo)
@@ -145,22 +147,14 @@ func GetAscendDeviceNames() []string {
 	return deviceNames
 }
 
-func (ads *AscendDevices) AddResourceUsage(id string, cores int32, mem int32) error {
-	dev, ok := ads.Devices[id]
-	if !ok {
-		return fmt.Errorf("ascend device %s not found", id)
-	}
+func (ads *AscendDevices) AddResourceUsage(dev *AscendDevice, cores int32, mem int32) error {
 	dev.DeviceUsage.Used++
 	dev.DeviceUsage.Usedcores += cores
 	dev.DeviceUsage.Usedmem += mem
 	return nil
 }
 
-func (ads *AscendDevices) SubResourceUsage(id string, cores int32, mem int32) error {
-	dev, ok := ads.Devices[id]
-	if !ok {
-		return fmt.Errorf("ascend device %s not found", id)
-	}
+func (ads *AscendDevices) SubResourceUsage(dev *AscendDevice, cores int32, mem int32) error {
 	dev.DeviceUsage.Used--
 	dev.DeviceUsage.Usedcores -= cores
 	dev.DeviceUsage.Usedmem -= mem
@@ -178,7 +172,7 @@ func (ads *AscendDevices) SubResource(pod *v1.Pod) {
 	if ads == nil {
 		return
 	}
-	ano_key := devices.InRequestDevices[ads.Type]
+	ano_key := devices.SupportDevices[ads.Type]
 	ano, ok := pod.Annotations[ano_key]
 	if !ok {
 		return
@@ -189,12 +183,21 @@ func (ads *AscendDevices) SubResource(pod *v1.Pod) {
 		return
 	}
 	for _, cono_dev := range con_devs {
-		ads.SubResourceUsage(cono_dev.UUID, cono_dev.Usedcores, cono_dev.Usedmem)
+		dev, ok := ads.Devices[cono_dev.UUID]
+		if !ok {
+			klog.Warningf("ascend device %s not found", cono_dev.UUID)
+			continue
+		}
+		if _, ok := dev.PodMap[string(pod.UID)]; ok {
+			delete(dev.PodMap, string(pod.UID))
+			ads.SubResourceUsage(dev, cono_dev.Usedcores, cono_dev.Usedmem)
+			klog.V(5).Infof("sub resource usage for pod %s. device %s usedmem %d", pod.Name, dev.DeviceInfo.ID, cono_dev.Usedmem)
+		}
 	}
 }
 
 func (ads *AscendDevices) addResource(annotations map[string]string, pod *v1.Pod) {
-	ano_key := devices.InRequestDevices[ads.Type]
+	ano_key := devices.SupportDevices[ads.Type]
 	ano, ok := annotations[ano_key]
 	if !ok {
 		return
@@ -205,7 +208,20 @@ func (ads *AscendDevices) addResource(annotations map[string]string, pod *v1.Pod
 		return
 	}
 	for _, cono_dev := range con_devs {
-		ads.AddResourceUsage(cono_dev.UUID, cono_dev.Usedcores, cono_dev.Usedmem)
+		dev, ok := ads.Devices[cono_dev.UUID]
+		if !ok {
+			klog.Warningf("ascend device %s not found", cono_dev.UUID)
+			continue
+		}
+		if _, ok := dev.PodMap[string(pod.UID)]; !ok {
+			dev.PodMap[string(pod.UID)] = &devices.DeviceUsage{
+				Used:      1,
+				Usedcores: cono_dev.Usedcores,
+				Usedmem:   cono_dev.Usedmem,
+			}
+			ads.AddResourceUsage(dev, cono_dev.Usedcores, cono_dev.Usedmem)
+			klog.V(5).Infof("add resource usage for pod %s. device %s usedmem %d", pod.Name, dev.DeviceInfo.ID, cono_dev.Usedmem)
+		}
 	}
 }
 
@@ -484,7 +500,7 @@ func fit(req *devices.ContainerDeviceRequest, dev *AscendDevice) bool {
 func getDeviceSnapshot(ads *AscendDevices) []*AscendDevice {
 	dupDevs := make([]*AscendDevice, 0, len(ads.Devices))
 	for _, dev := range ads.Devices {
-		dup_dev := &AscendDevice{
+		dupDev := &AscendDevice{
 			config:           dev.config,
 			nodeRegisterAnno: dev.nodeRegisterAnno,
 			useUUIDAnno:      dev.useUUIDAnno,
@@ -495,8 +511,16 @@ func getDeviceSnapshot(ads *AscendDevices) []*AscendDevice {
 				Usedmem:   dev.DeviceUsage.Usedmem,
 				Usedcores: dev.DeviceUsage.Usedcores,
 			},
+			PodMap: make(map[string]*devices.DeviceUsage),
 		}
-		dupDevs = append(dupDevs, dup_dev)
+		for k, v := range dev.PodMap {
+			dupDev.PodMap[k] = &devices.DeviceUsage{
+				Used:      v.Used,
+				Usedmem:   v.Usedmem,
+				Usedcores: v.Usedcores,
+			}
+		}
+		dupDevs = append(dupDevs, dupDev)
 	}
 	return dupDevs
 }
