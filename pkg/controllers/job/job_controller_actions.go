c214fe1b36099d00e9cfad855fe29b1a4036a250
Avinash Kumar Deepak <avinash8655279@gmail.com>
2026-06-10 15:16:48 +0530

    Fix slice retention memory leak in appendJobCondition
    Allocates a new slice and copies elements instead of slicing the existing array, ensuring old pointers are safely garbage collected.
    
    Signed-off-by: Avinash Kumar Deepak <avinash8655279@gmail.com>

--- pkg/controllers/job/job_controller_actions.go
+++ pkg/controllers/job/job_controller_actions.go
@@ -1077,7 +1077,9 @@ func appendJobCondition(conditions []batch.JobCondition, c batch.JobCondition) [
 	}
 	conditions = append(conditions, c)
 	if len(conditions) > maxJobConditions {
-		conditions = conditions[len(conditions)-maxJobConditions:]
+		newConditions := make([]batch.JobCondition, maxJobConditions)
+		copy(newConditions, conditions[len(conditions)-maxJobConditions:])
+		return newConditions
 	}
 	return conditions
 }
