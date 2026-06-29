03f2a0b995eec9f2052cdbb18efedb3ef72b06bb
JesseStutler <chenzicong4@huawei.com>
2026-06-29 03:53:42 +0000

    fix(predicates): register more PreFilter plugins
    Signed-off-by: JesseStutler <chenzicong4@huawei.com>

--- pkg/scheduler/plugins/predicates/predicates.go
+++ pkg/scheduler/plugins/predicates/predicates.go
@@ -525,6 +525,7 @@ func (pp *PredicatesPlugin) InitPlugin() {
 			nodeAffinityFilter := plugin.(*nodeaffinity.NodeAffinity)
 			addFilterPlugin(nodeaffinity.Name, nodeAffinityFilter)
 			addStableFilterPlugin(nodeaffinity.Name, nodeAffinityFilter)
+			addPreFilterPlugin(nodeaffinity.Name, nodeAffinityFilter)
 		} else {
 			klog.Errorf("Failed to init %s plugin %v", nodeaffinity.Name, err)
 		}
@@ -575,6 +576,7 @@ func (pp *PredicatesPlugin) InitPlugin() {
 		if plugin, err := volumezone.New(context.TODO(), nil, pp.Handle, pp.features); err == nil {
 			volumeZoneFilter := plugin.(*volumezone.VolumeZone)
 			addFilterPlugin(volumezone.Name, volumeZoneFilter)
+			addPreFilterPlugin(volumezone.Name, volumeZoneFilter)
 		} else {
 			klog.Errorf("Failed to init %s plugin %v", volumezone.Name, err)
 		}
