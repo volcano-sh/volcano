6783bcdd216ceaa7d6a453935e0405cb8702a211
Avinash Kumar Deepak <avinash8655279@gmail.com>
2026-07-31 15:38:56 +0530

    fix: ignore scoped resource quotas
    Signed-off-by: Avinash Kumar Deepak <avinash8655279@gmail.com>

--- pkg/scheduler/api/namespace_info.go
+++ pkg/scheduler/api/namespace_info.go
@@ -28,20 +28,23 @@ type NamespaceInfo struct {
 	// Name is the name of this namespace
 	Name NamespaceName
 	// QuotaStatus stores the ResourceQuotaStatus of all ResourceQuotas in this namespace
-	QuotaStatus map[string]v1.ResourceQuotaStatus
+	QuotaStatus  map[string]v1.ResourceQuotaStatus
+	scopedQuotas map[string]bool
 }
 
 // NamespaceCollection will record all details about namespace
 type NamespaceCollection struct {
-	Name        string
-	QuotaStatus map[string]v1.ResourceQuotaStatus
+	Name         string
+	QuotaStatus  map[string]v1.ResourceQuotaStatus
+	scopedQuotas map[string]bool
 }
 
 // NewNamespaceCollection creates new NamespaceCollection object to record all information about a namespace
 func NewNamespaceCollection(name string) *NamespaceCollection {
 	n := &NamespaceCollection{
-		Name:        name,
-		QuotaStatus: make(map[string]v1.ResourceQuotaStatus),
+		Name:         name,
+		QuotaStatus:  make(map[string]v1.ResourceQuotaStatus),
+		scopedQuotas: make(map[string]bool),
 	}
 	return n
 }
@@ -49,11 +52,14 @@ func NewNamespaceCollection(name string) *NamespaceCollection {
 // Update modify the registered information according quota object
 func (n *NamespaceCollection) Update(quota *v1.ResourceQuota) {
 	n.QuotaStatus[quota.Name] = quota.Status
+	n.scopedQuotas[quota.Name] = len(quota.Spec.Scopes) > 0 ||
+		(quota.Spec.ScopeSelector != nil && len(quota.Spec.ScopeSelector.MatchExpressions) > 0)
 }
 
 // Delete remove the registered information according quota object
 func (n *NamespaceCollection) Delete(quota *v1.ResourceQuota) {
 	delete(n.QuotaStatus, quota.Name)
+	delete(n.scopedQuotas, quota.Name)
 }
 
 // Snapshot will clone a NamespaceInfo without Heap according NamespaceCollection
@@ -62,8 +68,18 @@ func (n *NamespaceCollection) Snapshot() *NamespaceInfo {
 	for k, v := range n.QuotaStatus {
 		copiedQuotaStatus[k] = *v.DeepCopy()
 	}
-	return &NamespaceInfo{
-		Name:        NamespaceName(n.Name),
-		QuotaStatus: copiedQuotaStatus,
+	copiedScopedQuotas := make(map[string]bool, len(n.scopedQuotas))
+	for k, v := range n.scopedQuotas {
+		copiedScopedQuotas[k] = v
 	}
+	return &NamespaceInfo{
+		Name:         NamespaceName(n.Name),
+		QuotaStatus:  copiedQuotaStatus,
+		scopedQuotas: copiedScopedQuotas,
+	}
+}
+
+// IsQuotaScoped returns whether a ResourceQuota has scopes or a scope selector.
+func (n *NamespaceInfo) IsQuotaScoped(name string) bool {
+	return n.scopedQuotas[name]
 }
