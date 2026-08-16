8b18cac81c3d91b2bfe26da25e740a32e0fe48f7
Talha Amjad <myown4500@gmail.com>
2026-08-16 10:27:26 +0500

    test(preempt): benchmark normal preemption predicate recheck
    Signed-off-by: Talha Amjad <myown4500@gmail.com>

--- pkg/scheduler/actions/preempt/preempt_test.go
+++ pkg/scheduler/actions/preempt/preempt_test.go
@@ -53,6 +53,8 @@ func init() {
 
 const deviceFitAfterEvictionPluginName = "device-fit-after-eviction"
 
+const normalPreemptBenchmarkPredicatePluginName = "normal-preempt-benchmark-predicate"
+
 type deviceFitAfterEvictionPlugin struct{}
 
 func (p *deviceFitAfterEvictionPlugin) Name() string {
@@ -79,6 +81,20 @@ func (p *deviceFitAfterEvictionPlugin) OnSessionOpen(ssn *framework.Session) {
 }
 
 func (p *deviceFitAfterEvictionPlugin) OnSessionClose(ssn *framework.Session) {}
+
+type normalPreemptBenchmarkPredicatePlugin struct{}
+
+func (p *normalPreemptBenchmarkPredicatePlugin) Name() string {
+	return normalPreemptBenchmarkPredicatePluginName
+}
+
+func (p *normalPreemptBenchmarkPredicatePlugin) OnSessionOpen(ssn *framework.Session) {
+	ssn.AddPredicateFn(p.Name(), func(task *api.TaskInfo, node *api.NodeInfo) error {
+		return nil
+	})
+}
+
+func (p *normalPreemptBenchmarkPredicatePlugin) OnSessionClose(ssn *framework.Session) {}
 
 func TestPreempt(t *testing.T) {
 	plugins := map[string]framework.PluginBuilder{
@@ -454,6 +470,19 @@ func TestPreempt(t *testing.T) {
 }
 
 func TestNormalPreemptRechecksPredicateAfterEviction(t *testing.T) {
+	test, tiers := newNormalPreemptDeviceFitFixture()
+	test.RegisterSession(tiers, []conf.Configuration{{
+		Name:      New().Name(),
+		Arguments: map[string]interface{}{EnableTopologyAwarePreemptionKey: false},
+	}})
+	defer test.Close()
+	test.Run([]framework.Action{New()})
+	if err := test.CheckAll(0); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func newNormalPreemptDeviceFitFixture() (uthelper.TestCommonStruct, []conf.Tier) {
 	plugins := map[string]framework.PluginBuilder{
 		conformance.PluginName: conformance.New,
 		gang.PluginName:        gang.New,
@@ -498,15 +527,88 @@ func TestNormalPreemptRechecksPredicateAfterEviction(t *testing.T) {
 		},
 	}}
 
-	test.RegisterSession(tiers, []conf.Configuration{{
+	return test, tiers
+}
+
+func newNormalPreemptBenchmarkFixture() (uthelper.TestCommonStruct, []conf.Tier) {
+	test, tiers := newNormalPreemptDeviceFitFixture()
+	test.Name = "normal preemption benchmark with one required victim"
+	test.Nodes = []*v1.Node{
+		util.BuildNode("n1", api.BuildResourceList("1", "1G", []api.ScalarResource{{Name: "pods", Value: "10"}}...), make(map[string]string)),
+	}
+	test.Plugins[normalPreemptBenchmarkPredicatePluginName] = func(framework.Arguments) framework.Plugin {
+		return &normalPreemptBenchmarkPredicatePlugin{}
+	}
+
+	benchmarkPlugins := make([]conf.PluginOption, 0, len(tiers[0].Plugins))
+	for _, plugin := range tiers[0].Plugins {
+		if plugin.Name != deviceFitAfterEvictionPluginName {
+			benchmarkPlugins = append(benchmarkPlugins, plugin)
+		}
+	}
+	trueValue := true
+	benchmarkPlugins = append(benchmarkPlugins, conf.PluginOption{
+		Name:             normalPreemptBenchmarkPredicatePluginName,
+		EnabledPredicate: &trueValue,
+	})
+	tiers[0].Plugins = benchmarkPlugins
+
+	return test, tiers
+}
+
+// BenchmarkNormalPreemptPredicateRecheck measures the direct normalPreempt
+// call when both revisions must evict one victim.
+func BenchmarkNormalPreemptPredicateRecheck(b *testing.B) {
+	previousVerbosity := flag.Lookup("v").Value.String()
+	if err := flag.Set("v", "0"); err != nil {
+		b.Fatal(err)
+	}
+	b.Cleanup(func() {
+		_ = flag.Set("v", previousVerbosity)
+	})
+
+	test, tiers := newNormalPreemptBenchmarkFixture()
+	ssn := test.RegisterSession(tiers, []conf.Configuration{{
 		Name:      New().Name(),
 		Arguments: map[string]interface{}{EnableTopologyAwarePreemptionKey: false},
 	}})
 	defer test.Close()
-	test.Run([]framework.Action{New()})
-	if err := test.CheckAll(0); err != nil {
-		t.Fatal(err)
+
+	preemptor := benchmarkPreemptor(ssn)
+	if preemptor == nil {
+		b.Fatal("benchmark preemptor was not found")
 	}
+	node := ssn.Nodes["n1"]
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for iteration := 0; iteration < b.N; iteration++ {
+		stmt := framework.NewStatement(ssn)
+
+		assigned, err := New().normalPreempt(ssn, stmt, preemptor, func(task *api.TaskInfo) bool {
+			return api.PreemptableStatus(task.Status) && task.Preemptable && task.Job != preemptor.Job
+		}, []*api.NodeInfo{node})
+
+		if err != nil {
+			b.Fatal(err)
+		}
+		if !assigned {
+			b.Fatal("normal preemption did not assign the preemptor")
+		}
+		stmt.Discard()
+	}
+}
+
+func benchmarkPreemptor(ssn *framework.Session) *api.TaskInfo {
+	for _, jobInfo := range ssn.Jobs {
+		for _, task := range jobInfo.TaskStatusIndex[api.Pending] {
+			if task.Name == "preemptor" {
+				return task
+			}
+		}
+	}
+
+	return nil
 }
 
 func TestTopologyAwarePreempt(t *testing.T) {
